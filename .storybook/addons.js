5cbfcfd16e74a019937a5067b397f6b10e7474f0
Evgeniy Troynov <etroynov@users.noreply.github.com>
2019-12-30 16:13:30 +0300

    Update addons.js

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,4 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-knobs/register';
-import 'storybook-addon-designs/register'
+import 'storybook-addon-designs/register';
