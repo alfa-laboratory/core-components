a8b4f24bef8d3b45de1d98b78809ca75e079a16b
Evgeniy Troynov <troinof@gmail.com>
2019-12-31 20:16:30 +0300

    feature/COMPONENTS-AMOUNT. add amount component

---
+++ packages/utils/getCurrencySymbol.ts
@@ -0,0 +1,15 @@
+/**
+ * Config
+ */
+
+import { CURRENCY_CODES } from '../configs/currencyCodes';
+
+/**
+ * Возвращает знак валюты по ISO коду.
+ *
+ * @param {String} currencyCode Код валюты.
+ * @returns {String}
+ */
+
+export const getCurrencySymbol = (currencyCode: string): string =>
+  CURRENCY_CODES[currencyCode] ?? currencyCode;
