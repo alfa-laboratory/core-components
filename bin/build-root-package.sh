61499310c0f928077b887e19621fc50989bac9dc
iLya Raylyan <IRaylyan@alfabank.ru>
2020-02-04 16:27:45 +0300

    feat(*): add comments to bash

--- bin/build-root-package.sh
+++ bin/build-root-package.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# название подпакета => @alfalab/core-components-button => button = package_name
 package_name=$(echo $1 | awk -F- '{print $3}')
+# создаю дерикторию в корне проекта
 mkdir -p ../../dist/$package_name
+# копирую собранный подпакет в корневую сборку
 cp -r dist/ ../../dist/$package_name
--- bin/publish.sh
+++ bin/publish.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# поднимаю версию во всех подпакетах
 lerna version --no-push
+# собираю корневой проект
 yarn build
+# публикую все подпакеты
 lerna publish from-git --contents dist
+# публикую корневой проект
 yarn publish dist --no-git-tag-version
+# копирую package.json, чтобы сохранить поднятую, во публикации, версию
 cp dist/package.json package.json
+# сохраняю в гите поднятую версию
 git add package.json
 git commit -m 'update version'
 git push
