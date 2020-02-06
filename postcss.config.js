c81ad6e225324c38052fd8aae37001be0ef5c756
iLya Raylyan <IRaylyan@alfabank.ru>
2020-02-06 19:47:42 +0300

    feat(*): modify config for storybook

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  plugins: [
-    require('postcss-import')({}),
-    require('postcss-preset-env')({
+  plugins: {
+  'postcss-import': {},
+  'postcss-preset-env': {
       stage: 3,
       features: {
         'nesting-rules': true,
       },
-    }),
-  ],
+    },
+  },
 };
