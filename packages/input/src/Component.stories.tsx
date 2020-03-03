2314b3b6f47e7b4ebfb3718927ecd593d33b2e1b
reme3d2y <reme3d2y@gmail.com>
2020-03-03 18:32:52 +0300

    feat(input): add new default input

---
+++ packages/input/src/Component.stories.tsx
@@ -0,0 +1,47 @@
+/**
+ * Vendor
+ */
+
+import { withKnobs, select, text, boolean } from '@storybook/addon-knobs';
+import React, { useState } from 'react';
+import { withDesign } from 'storybook-addon-designs';
+/**
+ * Components
+ */
+import { Input } from './Component';
+
+
+
+export default {
+    title: 'Common',
+    component: Input,
+    decorators: [withDesign, withKnobs]
+};
+
+export const InputStory = () => {
+    const [value, setValue] = useState('value');
+
+    return (
+        <Input
+            size={ select('size', ['s', 'm', 'l'], 's') }
+            disabled={ boolean('Disabled', false) }
+            placeholder={ text('placeholder', '') }
+            label={ text('label', '') }
+            hint={ text('hint', '') }
+            error={ text('error', '') }
+            value={ value }
+            onChange={ (e: any) => setValue(e.target.value) }
+        />
+    );
+};
+
+InputStory.story = {
+    name: 'Input',
+    parameters: {
+        design: {
+            type: 'figma',
+            // public link for testing
+            url: ''
+        }
+    }
+};
---
+++ packages/input/src/Component.tsx
@@ -0,0 +1,86 @@
+/**
+ * Vendor
+ */
+
+import React from 'react';
+import cn from 'classnames';
+
+/**
+ * Styles
+ */
+
+import styles from './Component.module.css';
+import { BaseInput, BaseInputProps } from '../../base-input/src';
+
+/**
+ * Types
+ */
+
+type InputProps = BaseInputProps & {
+    /** Текст подсказки */
+    hint?: 'string';
+    /** Текст ошибки */
+    error?: 'string';
+};
+
+/**
+ * Expo
+ */
+
+export const Input: React.FC<InputProps> = ({
+    size,
+    children,
+    className,
+    innerClassName,
+    inputClassName,
+    value,
+    label,
+    placeholder,
+    hint,
+    error,
+    disabled,
+    onChange,
+    onFocus,
+    onBlur,
+    dataTestId
+}) => {
+    const hasHint = !!hint;
+    const hasError = !!error;
+    const hasSub = hasHint || hasError;
+
+    return (
+        <div
+            className={ cn(
+                styles.component,
+                {
+                    [styles.hasError]: hasError
+                },
+                className
+            ) }
+            data-test-id={ dataTestId }
+        >
+            <BaseInput
+                className={ cn(styles.baseInput) }
+                innerClassName={ cn(styles.inner, innerClassName) }
+                inputClassName={ cn(styles.input, inputClassName) }
+                size={ size }
+                value={ value }
+                label={ label }
+                placeholder={ placeholder }
+                disabled={ disabled }
+                onChange={ onChange }
+                onFocus={ onFocus }
+                onBlur={ onBlur }
+            />
+
+            { hasSub && (
+                <span className={ cn(styles.sub) }>
+                    { (hasHint && !hasError) && hint }
+                    { hasError && error }
+                </span>
+            ) }
+
+            { children }
+        </div>
+    );
+};
