86fa36694323671bd3e4bbb28980134d4e7e2ce2
reme3d2y <reme3d2y@gmail.com>
2020-03-04 18:02:34 +0300

    fix(*): fix lint errors

--- packages/base-input/src/Component.stories.tsx
+++ packages/base-input/src/Component.stories.tsx
@@ -3,7 +3,7 @@
  */
 
 import {
-    withKnobs, select, text, boolean
+  withKnobs, select, text, boolean
 } from '@storybook/addon-knobs';
 import React, { useState } from 'react';
 import { withDesign } from 'storybook-addon-designs';
@@ -13,34 +13,34 @@ import { withDesign } from 'storybook-addon-designs';
 import { BaseInput } from './Component';
 
 export default {
-    title: 'Common',
-    component: BaseInput,
-    decorators: [withDesign, withKnobs]
+  title: 'Common',
+  component: BaseInput,
+  decorators: [withDesign, withKnobs]
 };
 
 export const BaseInputStory = () => {
-    const [value, setValue] = useState('value');
+  const [value, setValue] = useState('value');
 
-    return (
-        <BaseInput
-            type={ select('type', ['text', 'number', 'card', 'email', 'file', 'hidden', 'money', 'password', 'tel', 'text'], 'text') }
-            size={ select('size', ['s', 'm', 'l'], 's') }
-            disabled={ boolean('Disabled', false) }
-            placeholder={ text('placeholder', '') }
-            label={ text('label', '') }
-            value={ value }
-            onChange={ (e: any) => setValue(e.target.value) }
-        />
-    );
+  return (
+    <BaseInput
+      type={ select('type', ['text', 'number', 'card', 'email', 'file', 'hidden', 'money', 'password', 'tel', 'text'], 'text') }
+      size={ select('size', ['s', 'm', 'l'], 's') }
+      disabled={ boolean('Disabled', false) }
+      placeholder={ text('placeholder', '') }
+      label={ text('label', '') }
+      value={ value }
+      onChange={ (e: any) => setValue(e.target.value) }
+    />
+  );
 };
 
 BaseInputStory.story = {
-    name: 'BaseInput',
-    parameters: {
-        design: {
-            type: 'figma',
-            // public link for testing
-            url: ''
-        }
+  name: 'BaseInput',
+  parameters: {
+    design: {
+      type: 'figma',
+      // public link for testing
+      url: ''
     }
+  }
 };
--- packages/base-input/src/Component.tsx
+++ packages/base-input/src/Component.tsx
@@ -59,102 +59,101 @@ export type BaseInputProps = {
  */
 
 export const BaseInput = React.forwardRef<HTMLInputElement, BaseInputProps>(({
-    size='s',
-    type='text',
-    className,
-    innerClassName,
-    inputClassName,
-    value,
-    disabled,
-    required,
-    placeholder,
-    label,
-    leftAddons,
-    rightAddons,
-    children,
-    onFocus,
-    onBlur,
-    onChange,
-    dataTestId
+  size='s',
+  type='text',
+  className,
+  innerClassName,
+  inputClassName,
+  value,
+  disabled,
+  required,
+  placeholder,
+  label,
+  leftAddons,
+  rightAddons,
+  children,
+  onFocus,
+  onBlur,
+  onChange,
+  dataTestId
 }, ref) => {
-    const [focused, setFocused] = useState(false);
+  const [focused, setFocused] = useState(false);
 
-    const filled = !!value;
-    const hasLabel = !!label;
+  const filled = !!value;
+  const hasLabel = !!label;
 
-    function handleInputFocus(e: React.FocusEvent<HTMLInputElement>) {
-        setFocused(true);
+  function handleInputFocus(e: React.FocusEvent<HTMLInputElement>) {
+    setFocused(true);
 
-        if (onFocus) {
-            onFocus(e);
-        }
+    if (onFocus) {
+      onFocus(e);
     }
+  }
 
-    function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {
-        setFocused(false);
+  function handleInputBlur(e: React.FocusEvent<HTMLInputElement>) {
+    setFocused(false);
 
-        if (onBlur) {
-            onBlur(e);
-        }
+    if (onBlur) {
+      onBlur(e);
     }
+  }
 
-    function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
-        if (onChange) {
-            onChange(e);
-        }
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (onChange) {
+      onChange(e);
     }
+  }
 
-    return (
-        <div
-            className={ cn(
-                styles.component,
-                {
-                    [styles.focused]: focused,
-                    [styles.disabled]: disabled,
-                    [styles.filled]: filled,
-                    [styles.hasLabel]: hasLabel
-                },
-                className
-            ) }
-        >
-            <div className={ cn(styles.inner, innerClassName) }>
-                { leftAddons && (
-                    <div className={ cn(styles.addons, styles.leftAddons) }>
-                        { leftAddons }
-                    </div>
-                ) }
+  return (
+    <div
+      className={ cn(
+        styles.component,
+        {
+          [styles.focused]: focused,
+          [styles.disabled]: disabled,
+          [styles.filled]: filled,
+          [styles.hasLabel]: hasLabel
+        },
+        className
+      ) }
+    >
+      <div className={ cn(styles.inner, innerClassName) }>
+        { leftAddons && (
+          <div className={ cn(styles.addons, styles.leftAddons) }>
+            { leftAddons }
+          </div>
+        ) }
 
-
-                <div className={ cn(styles.inputWrapper) }>
-                    { label && (
-                        <div className={ cn(styles.label) }>
-                            { label }
-                        </div>
-                    ) }
-
-                    <input
-                        className={ cn(styles.input, styles[size], inputClassName) }
-                        ref={ ref }
-                        type={ type }
-                        value={ value }
-                        placeholder={ placeholder }
-                        disabled={ disabled }
-                        required={ required }
-                        onChange={ handleInputChange }
-                        onFocus={ handleInputFocus }
-                        onBlur={ handleInputBlur }
-                        data-test-id={ dataTestId }
-                    />
-                </div>
-
-                { rightAddons && (
-                    <div className={ cn(styles.addons, styles.rightAddons) }>
-                        { rightAddons }
-                    </div>
-                ) }
+        <div className={ cn(styles.inputWrapper) }>
+          { label && (
+            <div className={ cn(styles.label) }>
+              { label }
             </div>
+          ) }
 
-            { children }
+          <input
+            className={ cn(styles.input, styles[size], inputClassName) }
+            ref={ ref }
+            type={ type }
+            value={ value }
+            placeholder={ placeholder }
+            disabled={ disabled }
+            required={ required }
+            onChange={ handleInputChange }
+            onFocus={ handleInputFocus }
+            onBlur={ handleInputBlur }
+            data-test-id={ dataTestId }
+          />
         </div>
-    );
+
+        { rightAddons && (
+          <div className={ cn(styles.addons, styles.rightAddons) }>
+            { rightAddons }
+          </div>
+        ) }
+      </div>
+
+      { children }
+    </div>
+  );
 });
