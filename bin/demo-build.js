1e349b363fd96d6d4f429596f4a465b5bf1fc456
reme3d2y <AYatsenko@alfabank.ru>
2020-03-19 17:38:24 +0300

    fix(*): add /bin to .eslintignore

--- bin/demo-build.js
+++ bin/demo-build.js
@@ -5,8 +5,8 @@
  */
 
 /* eslint-disable no-console */
-import { exec, rm, mkdir, cd, cp } from 'shelljs';
-import parseGitUrl from 'git-url-parse';
+const shell = require('shelljs');
+const parseGitUrl = require('git-url-parse');
 
 /** Config for github */
 const defaultConfig = {
@@ -24,56 +24,55 @@ const execOptions = {
     fatal: true,
 };
 /** Temporary dir for builded file = last git commit hash */
-const tempOutputDir = exec('git rev-parse HEAD', execOptions).stdout.trim();
+const tempOutputDir = shell.exec('git rev-parse HEAD', execOptions).stdout.trim();
 /** Current git branch */
-const sourceBranch = exec('git rev-parse --abbrev-ref HEAD', execOptions).stdout.trim();
+const sourceBranch = shell.exec('git rev-parse --abbrev-ref HEAD', execOptions).stdout.trim();
 /** Git remote url */
-const gitUrl = exec(
-    `git config --get remote.${defaultConfig.gitRemote}.url`,
-    execOptions,
-).stdout.trim();
+const gitUrl = shell
+    .exec(`git config --get remote.${defaultConfig.gitRemote}.url`, execOptions)
+    .stdout.trim();
 /** Parsed git url */
 const parsedGitUrl = parseGitUrl(gitUrl);
 const gitPagesUrl = `https://${parsedGitUrl.owner}.github.io/${parsedGitUrl.name}`;
 
 console.log('Publish storybook demo for github');
 console.log('=> Build storybook');
-exec(`yarn build-storybook -o ${tempOutputDir}`, { fatal: true });
+shell.exec(`yarn build-storybook -o ${tempOutputDir}`, { fatal: true });
 
 // Prepare temporary gh-pages dir
 console.log('=> Prepare temporary dir');
-rm('-rf', `./${ghMergeDir}`);
-mkdir(ghMergeDir);
+shell.rm('-rf', `./${ghMergeDir}`);
+shell.mkdir(ghMergeDir);
 
 // Go to the temporary directory and create a *new* Git repo
-cd(ghMergeDir);
-exec('git init', execOptions);
+shell.cd(ghMergeDir);
+shell.exec('git init', execOptions);
 // Inside this git repo we'll pretend to be a new user
-exec(`git config user.name "${defaultConfig.gitUsername}"`, execOptions);
-exec(`git config user.email "${defaultConfig.gitEmail}"`, execOptions);
+shell.exec(`git config user.name "${defaultConfig.gitUsername}"`, execOptions);
+shell.exec(`git config user.email "${defaultConfig.gitEmail}"`, execOptions);
 
 // Disable GPG signing
-exec('git config commit.gpgsign false', execOptions);
+shell.exec('git config commit.gpgsign false', execOptions);
 
 // Pull gh-page file
 console.log('=> Pull storybook file');
-exec(`git pull -f -q ${gitUrl} ${defaultConfig.targetBranch}`, execOptions);
+shell.exec(`git pull -f -q ${gitUrl} ${defaultConfig.targetBranch}`, execOptions);
 
 // Merge builded storybook
 console.log('=> Merge builded storybook');
-cd('../');
+shell.cd('../');
 if (sourceBranch === 'master') {
-    cp('-rf', `./${tempOutputDir}`, `./${ghMergeDir}/master`);
+    shell.cp('-rf', `./${tempOutputDir}`, `./${ghMergeDir}/master`);
 } else {
-    cp('-rf', `./${tempOutputDir}`, `./${ghMergeDir}`);
+    shell.cp('-rf', `./${tempOutputDir}`, `./${ghMergeDir}`);
 }
-cd(ghMergeDir);
+shell.cd(ghMergeDir);
 
 // The first and only commit to this new Git repo contains all the
 // files present with the commit message "Deploy to GitHub Pages".
 console.log(`=> Commit changes with message: ${defaultConfig.commitMessage}`);
-exec('git add .', execOptions);
-exec(`git commit -m "${defaultConfig.commitMessage}"`, execOptions);
+shell.exec('git add .', execOptions);
+shell.exec(`git commit -m "${defaultConfig.commitMessage}"`, execOptions);
 
 // log output url
 if (sourceBranch === 'master') {
