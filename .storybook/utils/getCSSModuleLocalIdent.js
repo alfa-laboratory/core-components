3a9a6524e2463d5687e8f3a9fd8fed4e7666310e
reme3d2y <AYatsenko@alfabank.ru>
2020-04-02 09:03:32 +0300

    refactor(getCSSModuleLocalIdent.js): refactor code

--- .storybook/utils/getCSSModuleLocalIdent.js
+++ .storybook/utils/getCSSModuleLocalIdent.js
@@ -1,18 +1,12 @@
-// Fork of https://github.com/facebook/create-react-app/blob/master/packages/react-dev-utils/getCSSModuleLocalIdent.js
-
 const loaderUtils = require('loader-utils');
 const path = require('path');
 
 module.exports = function getCSSModuleLocalIdent(context, localIdentName, localName, options) {
-    const matches = context.resourcePath.match(
-        /src\/(.*)\/src\/index\.module\.(css|scss|sass)$/,
-    );
+    // В случае компонентов - использует имя папки, в остальных - имя ресурса.
+    const matches = context.resourcePath.match(/src\/(.*)\/src\/index\.module\.css$/);
+    const componentOrFileName = matches ? matches[1] : '[name]';
 
-    // Use the filename or component folder name, based on some uses the index.js / index.module.(css|scss|sass) project style
-    // We store sources in /src folder, so [folder] === 'src'. For this reason, we should get component name from regexp.
-    const fileNameOrFolder = matches ? matches[1] : '[name]';
-
-    // Create a hash based on a the file location and class name. Will be unique across a project, and close to globally unique.
+    // Создает уникальный хэш, основанные на расположении файла и className.
     const hash = loaderUtils.getHashDigest(
         path.posix.relative(context.rootContext, context.resourcePath) + localName,
         'md5',
@@ -20,13 +14,10 @@ module.exports = function getCSSModuleLocalIdent(context, localIdentName, localN
         5,
     );
 
-    // Use loaderUtils to find the file or folder name
-    const className = loaderUtils.interpolateName(
+    // Интерполирует шаблон и составляет className.
+    return loaderUtils.interpolateName(
         context,
-        fileNameOrFolder + '_' + localName + '__' + hash,
+        componentOrFileName + '_' + localName + '--' + hash,
         options,
     );
-
-    // remove the .module that appears in every classname when based on the file.
-    return className.replace('.module_', '_');
 };
