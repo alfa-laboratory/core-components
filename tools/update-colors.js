827f97685ef769f4bc377ef32f30d9bd56e90628
reme3d2y <AYatsenko@alfabank.ru>
2020-04-02 08:28:58 +0300

    refactor(tools): small improvements & style

--- tools/update-colors.js
+++ tools/update-colors.js
@@ -4,8 +4,7 @@ const path = require('path');
 const UNDERSCORE_RE = /_/g;
 const DASH = '-';
 
-const colorsPath = path.resolve(__dirname, '../node_modules/alfa-ui-primitives/styles/colors.json');
-const colors = JSON.parse(fs.readFileSync(colorsPath, 'utf8'));
+const colors = require('../node_modules/alfa-ui-primitives/styles/colors.json');
 
 let css = '';
 
@@ -16,11 +15,6 @@ Object.entries(colors).forEach(([name, token]) => {
 
     const formatedName = name.replace(UNDERSCORE_RE, DASH);
     let value = token.a === 1 ? token.hex : token.rgba;
-
-    if (/#(\w)\1{5}/.test(value)) {
-        // #ffffff -> #fff;
-        value = value.slice(0, 4);
-    }
 
     css += `    --color-${formatedName}: ${value};\n`;
 });
--- tools/update-typography.js
+++ tools/update-typography.js
@@ -2,23 +2,19 @@ const fs = require('fs');
 const path = require('path');
 const handlebars = require('handlebars');
 
-const typographyPath = resolve(
-    __dirname,
-    '../node_modules/alfa-ui-primitives/styles/typography_web.json',
-);
-const typography = JSON.parse(fs.readFileSync(typographyPath, 'utf8'));
+const typography = require('../node_modules/alfa-ui-primitives/styles/typography_web.json');
 
-let out = [];
+const out = [
+    compileTemplate('font-preset-mixin.css.hbs')
+];
 
-out.push(compileTemplate('font-preset-mixin.css.hbs'));
-
-Object.entries(typography).forEach(([name, settings]) => {
-    if (settings.deprecated) return;
+Object.entries(typography).forEach(([name, rules]) => {
+    if (rules.deprecated) return;
 
     out.push(
         compileTemplate('font-style-mixin.css.hbs', {
             name,
-            ...normalizeSettings(settings),
+            ...normalizeRules(rules),
         }),
     );
 });
@@ -27,16 +23,27 @@ const cssPath = path.resolve(__dirname, '../src/vars/src/typography.css');
 
 fs.writeFileSync(cssPath, out.join('\n'));
 
-// FUNCTIONS
-
+/**
+ * Компилирует handlebars шаблон
+ *
+ * @param {string} filename
+ * @param {Object} data
+ * @returns {string}
+ */
 function compileTemplate(filename, data) {
     const templatePath = path.resolve(__dirname, `./templates/${filename}`);
     const template = handlebars.compile(fs.readFileSync(templatePath, 'utf8'));
     return template(data);
 }
 
-function normalizeSettings(settings) {
-    const rules = {
+/**
+ * Преобразовывает значения из набора в валидные css значения в соответствии с набором правил
+ *
+ * @param {Object} rules
+ * @returns {Object}
+ */
+function normalizeRules(rules) {
+    const convertRules = {
         font_weight: v => {
             if (v === 'medium') return 500;
             if (v === 'regular') return 'normal';
@@ -47,11 +54,11 @@ function normalizeSettings(settings) {
         },
     };
 
-    Object.entries(settings).forEach(([name, value]) => {
-        if (name in rules) {
-            settings[name] = rules[name](value);
+    Object.entries(rules).forEach(([name, value]) => {
+        if (name in convertRules) {
+            rules[name] = convertRules[name](value);
         }
     });
 
-    return settings;
+    return rules;
 }
