aeb9ab810c617c89cc38c2d79042af38cb239f4e
dmitrsavk <dmitrsavk@yandex.ru>
2020-04-07 15:36:36 +0300

    test(tag): add tests

--- src/tag/src/Component.test.tsx
+++ src/tag/src/Component.test.tsx
@@ -1,18 +1,126 @@
-/**
- * Vendor
- */
-
 import React from 'react';
-import { render } from '@testing-library/react';
-
-/**
- * Component
- */
+import { render, fireEvent } from '@testing-library/react';
 
 import { Tag } from './index';
 
-describe('Tag', () => {
+describe('Snapshots tests', () => {
     it('should match snapshot', () => {
+        expect(render(<Tag>Press me</Tag>)).toMatchSnapshot();
+    });
+
+    it('should match without childrens snapshot', () => {
         expect(render(<Tag />)).toMatchSnapshot();
+    });
+
+    it('should match with rigth addons snapshot', () => {
+        expect(render(<Tag rightAddons={<span>10 000 $</span>}>Press me</Tag>)).toMatchSnapshot();
+    });
+
+    it('should match with left addons snapshot', () => {
+        expect(render(<Tag leftAddons={<span>10 000 $</span>}>Press me</Tag>)).toMatchSnapshot();
+    });
+});
+
+describe('Classes tests', () => {
+    it('should set custom class', () => {
+        const className = 'custom-class';
+
+        const { container } = render(<Tag className={className} />);
+
+        expect(container.firstElementChild).toHaveClass(className);
+    });
+
+    it('should set size `s` as default size', () => {
+        const { container } = render(<Tag />);
+
+        expect(container.firstElementChild).toHaveClass('s');
+    });
+
+    it('should set `checked` class if props `checked` is presented', () => {
+        const { container } = render(<Tag checked={true} />);
+
+        expect(container.firstElementChild).toHaveClass('checked');
+    });
+});
+
+describe('Attributes tests', () => {
+    it('should set disabled attribute', () => {
+        const { container } = render(<Tag disabled={true} />);
+
+        expect(container.firstElementChild).toBeDisabled();
+    });
+});
+
+describe('Render tests', () => {
+    test('should unmount without errors', () => {
+        const { unmount } = render(<Tag rightAddons={<div>addons</div>}>Tag</Tag>);
+
+        expect(unmount).not.toThrowError();
+    });
+
+    test('should contain right addons', () => {
+        const rightAddonText = 'Right addon text';
+
+        const { container, getByText } = render(
+            <Tag rightAddons={<span>{rightAddonText}</span>}>Tag</Tag>,
+        );
+
+        expect(container.firstElementChild).toContainElement(getByText(rightAddonText));
+    });
+
+    test('should contain left addons', () => {
+        const leftAddonText = 'Left addon text';
+
+        const { container, getByText } = render(
+            <Tag leftAddons={<span>{leftAddonText}</span>}>Tag</Tag>,
+        );
+
+        expect(container.firstElementChild).toContainElement(getByText(leftAddonText));
+    });
+});
+
+describe('Interaction tests', () => {
+    test('should call `onClick` prop, if tag not disabled', () => {
+        const cb = jest.fn();
+
+        const { container } = render(<Tag onClick={cb}>Press me!</Tag>);
+
+        if (container.firstElementChild) {
+            fireEvent.click(container.firstElementChild);
+        }
+
+        expect(cb).toBeCalledTimes(1);
+    });
+
+    test('should do not call `onClick` prop, if tag is disabled', () => {
+        const cb = jest.fn();
+
+        const { container } = render(
+            <Tag onClick={cb} disabled={true}>
+                Press me!
+            </Tag>,
+        );
+
+        if (container.firstElementChild) {
+            fireEvent.click(container.firstElementChild);
+        }
+
+        expect(cb).toBeCalledTimes(0);
+    });
+
+    test('should do not call `onClick` prop, if tag is disabled and checked', () => {
+        const cb = jest.fn();
+
+        const { container } = render(
+            <Tag onClick={cb} disabled={true} checked={true}>
+                Press me!
+            </Tag>,
+        );
+
+        if (container.firstElementChild) {
+            fireEvent.click(container.firstElementChild);
+        }
+
+        expect(cb).toBeCalledTimes(0);
     });
 });
