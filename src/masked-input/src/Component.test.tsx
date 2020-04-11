1643373516c7e76b7a699b50e8c963ba08be5967
reme3d2y <AYatsenko@alfabank.ru>
2020-04-11 08:50:09 +0300

    test(masked-input): add tests

--- src/masked-input/src/Component.test.tsx
+++ src/masked-input/src/Component.test.tsx
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 /**
  * Component
@@ -11,8 +11,69 @@ import { render } from '@testing-library/react';
 
 import { MaskedInput } from './index';
 
-describe('Input', () => {
-    it('should match snapshot', () => {
-        expect(render(<MaskedInput />)).toMatchSnapshot();
+// prettier-ignore
+const cardMask = [/\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/, ' ', /\d/, /\d/, /\d/, /\d/];
+
+describe('MaskedInput', () => {
+    describe('Snapshots tests', () => {
+        it('should match snapshot', () => {
+            expect(render(<MaskedInput />)).toMatchSnapshot();
+        });
+    });
+
+    describe('Guide mode', () => {
+        it('should render empty input if `guide` not enabled', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<MaskedInput mask={cardMask} dataTestId={dataTestId} />);
+
+            const input = getByTestId(dataTestId) as HTMLInputElement;
+
+            expect(input.value).toBeFalsy();
+        });
+
+        it('should render empty input if `guide` enabled', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <MaskedInput mask={cardMask} guide={true} dataTestId={dataTestId} />,
+            );
+
+            const input = getByTestId(dataTestId) as HTMLInputElement;
+
+            expect(input.value).toBeFalsy();
+        });
+
+        it('should render filled input if `guide` and `showMask` enabled', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <MaskedInput
+                    mask={cardMask}
+                    guide={true}
+                    showMask={true}
+                    placeholderChar='_'
+                    dataTestId={dataTestId}
+                />,
+            );
+
+            const input = getByTestId(dataTestId) as HTMLInputElement;
+
+            expect(input.value).toBe('____ ____ ____ ____');
+        });
+    });
+
+    it('should format value according to mask', () => {
+        const dataTestId = 'test-id';
+        const { getByTestId } = render(<MaskedInput mask={cardMask} dataTestId={dataTestId} />);
+
+        const input = getByTestId(dataTestId) as HTMLInputElement;
+
+        fireEvent.change(input, { target: { value: 'before1234123412341234after±!@#$%^&*()_+' } });
+
+        expect(input.value).toBe('1234 1234 1234 1234');
+    });
+
+    it('should unmount without errors', () => {
+        const { unmount } = render(<MaskedInput />);
+
+        expect(unmount).not.toThrowError();
     });
 });
