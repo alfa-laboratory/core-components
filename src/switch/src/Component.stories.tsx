7281981cb7c5fa253e454a192eaaf0608ce3f930
reme3d2y <AYatsenko@alfabank.ru>
2020-04-14 08:52:19 +0300

    feat(switch): add new component

---
+++ src/switch/src/Component.stories.tsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import cn from 'classnames';
+
+import { withKnobs, text, boolean, select } from '@storybook/addon-knobs';
+
+import styles from '../../../.storybook/styles.css';
+
+import { Switch } from './Component';
+
+export default {
+    title: 'Common|Switch',
+    component: Switch,
+    decorators: [withKnobs],
+};
+
+export const SwitchStory = () => {
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+    return (
+        <React.Fragment>
+            <div className={cn(styles.row)}>
+                <div className={cn(styles.col)}>
+                    <Switch
+                        checked={checked}
+                        disabled={boolean('Disabled', false)}
+                        block={boolean('Block', false)}
+                        contentAlign={select('contentAlign', ['left', 'right'], 'right')}
+                        className={text('className', '')}
+                        dataTestId={text('dataTestId', '')}
+                        label={text('text', 'Условие тоггла')}
+                        hint={text('description', 'Описание пункта')}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <div className={cn(styles.row)}>
+                <div className={cn(styles.col)}>
+                    <Switch
+                        checked={checked}
+                        disabled={boolean('Disabled', false)}
+                        block={boolean('Block', false)}
+                        contentAlign={select('contentAlign', ['left', 'right'], 'right')}
+                        className={text('className', '')}
+                        dataTestId={text('dataTestId', '')}
+                        onChange={handleChange}
+                    >
+                        <em>Custom content</em>
+                    </Switch>
+                </div>
+            </div>
+
+            <div className={cn(styles.row)}>
+                <div className={cn(styles.col)}>
+                    <Switch
+                        checked={checked}
+                        disabled={boolean('Disabled', false)}
+                        block={boolean('Block', false)}
+                        contentAlign={select('contentAlign', ['left', 'right'], 'right')}
+                        className={text('className', '')}
+                        dataTestId={text('dataTestId', '')}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+        </React.Fragment>
+    );
+};
+
+SwitchStory.story = {
+    name: 'Switch',
+    parameters: {},
+};
---
+++ src/switch/src/Component.tsx
@@ -0,0 +1,105 @@
+import React, { InputHTMLAttributes, useCallback, ChangeEvent } from 'react';
+import cn from 'classnames';
+
+import styles from './index.module.css';
+
+export type SwitchProps = Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    'type' | 'hint' | 'onChange'
+> & {
+    /**
+     * Управление состоянием вкл/выкл компонента
+     */
+    checked?: boolean;
+
+    /**
+     * Текст подписи к переключателю
+     */
+    label?: string;
+
+    /**
+     * Текст подсказки снизу
+     */
+    hint?: string;
+
+    /**
+     * Растягивает компонент на ширину контейнера
+     */
+    block?: boolean;
+
+    /**
+     * Кастомный контент
+     */
+    children?: React.ReactNode;
+
+    /**
+     * Сторона, с которой будет отображаться контент
+     */
+    contentAlign?: 'left' | 'right';
+
+    /**
+     * Обработчик переключения компонента
+     */
+    onChange?: (event?: ChangeEvent<HTMLInputElement>, payload?: { checked: boolean }) => void;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+};
+
+export const Switch = ({
+    contentAlign = 'right',
+    checked = false,
+    disabled,
+    label,
+    hint,
+    children,
+    className,
+    onChange,
+    block,
+    dataTestId,
+    ...restProps
+}: SwitchProps) => {
+    const hasContent = label || hint || children;
+
+    const handleChange = useCallback(
+        (e: ChangeEvent<HTMLInputElement>) => {
+            if (onChange) {
+                onChange(e, { checked: e.target.checked });
+            }
+        },
+        [onChange],
+    );
+
+    return (
+        // eslint-disable-next-line jsx-a11y/label-has-associated-control
+        <label
+            className={cn(styles.component, className, {
+                [styles.disabled]: disabled,
+                [styles.checked]: checked,
+                [styles.block]: block,
+                [styles.alignLeft]: contentAlign === 'left',
+            })}
+        >
+            <input
+                type='checkbox'
+                onChange={handleChange}
+                disabled={disabled}
+                checked={checked}
+                data-test-id={dataTestId}
+                {...restProps}
+            />
+
+            <span className={styles.switch} />
+
+            {hasContent && (
+                <div className={styles.content}>
+                    {label && <span className={styles.label}>{label}</span>}
+                    {hint && <span className={styles.hint}>{hint}</span>}
+                    {children}
+                </div>
+            )}
+        </label>
+    );
+};
