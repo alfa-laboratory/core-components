f0f0fa9fdaa1e69788e85c40b02b181850a42a87
reme3d2y <AYatsenko@alfabank.ru>
2020-04-16 12:39:07 +0300

    fix(build): prevent postcss fail if no css files in folder

--- bin/build.sh
+++ bin/build.sh
@@ -16,8 +16,12 @@ lerna exec --parallel -- NODE_ENV=production babel \
 lerna exec --parallel -- rm -rf ./dist/__snapshots__
 # обрабатываю ts во всех подпакетах
 lerna exec --parallel -- tsc --p tsconfig.json
-# обрабатываю postcss во всех подпакетах
-lerna exec --parallel --ignore @alfalab/core-components-vars -- postcss dist/*.css -d dist
+# обрабатываю postcss в подпакетах, которые содержат css-файлы, за исключением core-components-vars
+postcss_cmd='
+if [ $(find . -type f -name "*.css" | wc -l) -gt 0 ];
+    then postcss dist/*.css -d dist;
+fi'
+lerna exec --parallel --ignore @alfalab/core-components-vars -- $postcss_cmd
 # копирую package.json во всех подпакетах
 lerna exec --parallel -- cp package.json dist/package.json
 # удаляю папку dist в корне проекта
