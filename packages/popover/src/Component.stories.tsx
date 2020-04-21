225e9e8d7773f39e0ef0545fcc2406a01c29a05c
dmitrsavk <dmitrsavk@yandex.ru>
2020-04-21 16:29:24 +0300

    feat(popover): add new popover component

---
+++ packages/popover/src/Component.stories.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from 'react';
+
+import { withKnobs, select, number, boolean } from '@storybook/addon-knobs';
+import { Popover, Position } from './index';
+import { Button } from '../../button/src';
+
+export default {
+    title: 'Common|Popover',
+    component: Popover,
+    decorators: [withKnobs],
+};
+
+const POSITION_OPTIONS = [
+    'top',
+    'top-start',
+    'top-end',
+    'bottom',
+    'bottom-start',
+    'bottom-end',
+    'right',
+    'right-start',
+    'right-end',
+    'left',
+    'left-start',
+    'left-end',
+];
+
+export const Basic = () => {
+    const [open, setOpen] = useState(false);
+
+    const buttonRef = useRef<HTMLAnchorElement & HTMLButtonElement>(null);
+
+    const toggle = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div
+            style={{
+                width: '700px',
+                height: '300px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}
+        >
+            <Popover
+                anchorElement={buttonRef.current}
+                position={select('position', POSITION_OPTIONS, 'bottom') as Position}
+                open={open}
+                transition={{ timeout: 0 }}
+                offset={[number('offset[0]', 0), number('offset[1]', 0)]}
+                withArrow={boolean('withArrow', false)}
+            >
+                <div style={{ padding: '15px', width: '156px' }}>I am popover</div>
+            </Popover>
+
+            <Button ref={buttonRef} onClick={toggle}>
+                Show/Hide popover
+            </Button>
+        </div>
+    );
+};
+
+Basic.story = {
+    name: 'Popover',
+};
---
+++ packages/popover/src/Component.tsx
@@ -0,0 +1,169 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import cn from 'classnames';
+import { Transition } from 'react-transition-group';
+import { TransitionProps } from 'react-transition-group/Transition';
+import { usePopper } from 'react-popper';
+import { BasePlacement, VariationPlacement, Obj } from '@popperjs/core';
+
+import { Portal } from '@alfalab/core-components-portal';
+import styles from './index.module.css';
+
+type RefElement = HTMLElement | null;
+
+export type Position = BasePlacement | VariationPlacement;
+
+type PopperModifier = {
+    name: string;
+    options: Obj;
+};
+
+export type PopoverProps = {
+    /**
+     * Управление состоянием поповера (открыт/закрыт)
+     */
+    open: boolean;
+
+    /**
+     * Элемент, относительного которого появляется поповер
+     */
+    anchorElement: RefElement;
+
+    /**
+     * Позиционирование поповера
+     */
+    position?: Position;
+
+    /**
+     * Если `true`, будет отрисована стрелочка
+     */
+    withArrow?: boolean;
+
+    /**
+     * Смещение поповера
+     */
+    offset?: [number, number];
+
+    /**
+     * Дополнительные классы
+     */
+    classNames?: {
+        /**
+         * Дополнительный класс для стрелочки
+         */
+        popper?: string;
+
+        /**
+         * Дополнительный класс для поповера
+         */
+        arrow?: string;
+    };
+
+    /**
+     * Функция, возвращающая контейнер, в который будет рендериться поповер
+     */
+    getPortalContainer?: () => HTMLElement;
+
+    /**
+     * Transition props, прокидываются в компонент Transition.
+     * См. https://reactcommunity.org/react-transition-group/transition
+     */
+    transition?: TransitionProps;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+};
+
+const TRANSITION_DURATION = 300;
+
+export const Popover: React.FC<PopoverProps> = ({
+    children,
+    getPortalContainer,
+    transition = {},
+    anchorElement,
+    offset = [0, 0],
+    withArrow = false,
+    position = 'left',
+    classNames = {},
+    open,
+    dataTestId,
+}) => {
+    const [referenceElement, setReferenceElement] = useState<RefElement>(anchorElement);
+    const [popperElement, setPopperElement] = useState<RefElement>(null);
+    const [arrowElement, setArrowElement] = useState<RefElement>(null);
+
+    const getModifiers = useCallback(() => {
+        const modifiers: PopperModifier[] = [{ name: 'offset', options: { offset } }];
+
+        if (withArrow) {
+            modifiers.push({ name: 'arrow', options: { element: arrowElement } });
+        }
+
+        return modifiers;
+    }, [withArrow, arrowElement, offset]);
+
+    const { styles: popperStyles, attributes } = usePopper(referenceElement, popperElement, {
+        placement: position,
+        modifiers: getModifiers(),
+    });
+
+    useEffect(() => {
+        setReferenceElement(anchorElement);
+    }, [anchorElement]);
+
+    const getPopperClassName = useCallback(
+        status => cn(styles.component, styles[status], classNames.popper, status),
+        [classNames.popper],
+    );
+
+    const getArrowClassName = useCallback(() => cn(styles.arrow, classNames.arrow), [
+        classNames.arrow,
+    ]);
+
+    const timeout = transition.timeout === undefined ? TRANSITION_DURATION : transition.timeout;
+
+    const getTransitionProps = useCallback(() => {
+        const mountOnEnter = transition.mountOnEnter === undefined ? true : transition.mountOnEnter;
+        const unmountOnExit =
+            transition.unmountOnExit === undefined ? true : transition.unmountOnExit;
+
+        return {
+            ...transition,
+            in: open,
+            timeout,
+            mountOnEnter,
+            unmountOnExit,
+        };
+    }, [open, transition, timeout]);
+
+    const props = getTransitionProps();
+
+    return (
+        <Transition {...props}>
+            {status => (
+                <Portal getPortalContainer={getPortalContainer}>
+                    <div
+                        ref={setPopperElement}
+                        className={getPopperClassName(status)}
+                        style={{
+                            ...popperStyles.popper,
+                            transitionDuration: `${timeout}ms`,
+                        }}
+                        data-test-id={dataTestId}
+                        {...attributes.popper}
+                    >
+                        {children}
+                        {withArrow && (
+                            <div
+                                ref={setArrowElement}
+                                style={popperStyles.arrow}
+                                className={getArrowClassName()}
+                            />
+                        )}
+                    </div>
+                </Portal>
+            )}
+        </Transition>
+    );
+};
