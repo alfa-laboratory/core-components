afee7e36eb4c1416fc799b07d3a5189935e1e3dc
Evgeny Sergeev <siebensieben@gmail.com>
2020-04-22 17:30:30 +0300

    use @alfalab/icons in button story

--- packages/button/src/Component.stories.tsx
+++ packages/button/src/Component.stories.tsx
@@ -4,15 +4,10 @@ import cn from 'classnames';
 import { action } from '@storybook/addon-actions';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
 
+import Icon from '@alfalab/icons-glyph/StarMIcon';
 import styles from '../../../.storybook/styles.css';
 
 import { Button } from './Component';
-
-const icon = (
-    <svg width='24' height='24' fill='currentColor'>
-        <path d='M11.511 2.327a.528.528 0 01.978 0L15 8.967h6.474c.498 0 .716.619.325.923L16.5 14l2 7c.121.462-.403.825-.804.557L12 17.133l-5.696 4.424c-.4.268-.925-.095-.804-.557l2-7-5.299-4.11c-.391-.304-.173-.923.325-.923H9l2.511-6.64z' />
-    </svg>
-);
 
 export default {
     title: 'Common|Button',
@@ -46,14 +41,14 @@ export const ButtonStory = () => (
         </div>
         <div className={cn(styles.row)}>
             <div className={cn(styles.col)}>
-                <Button view='secondary' leftAddons={icon} size='m'>
+                <Button view='secondary' leftAddons={<Icon />} size='m'>
                     Secondary
                 </Button>
             </div>
         </div>
         <div className={cn(styles.row)}>
             <div className={cn(styles.col)}>
-                <Button view='secondary' rightAddons={icon} size='m' />
+                <Button view='secondary' rightAddons={<Icon />} size='m' />
             </div>
         </div>
         <div className={cn(styles.row)}>
