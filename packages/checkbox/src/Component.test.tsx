5704c717044a722a0a8daf0210aa5b5a38124630
Dmitry Savkin <dmitrsavk@yandex.ru>
2020-04-30 11:23:55 +0300

    Новый компонент Checkbox (#103)
    * feat(checkbox): add new component checkbox
    
    * feat(checkbox): fix active state
    
    * feat(checkbox): move div to span
    
    * feat(checkbox): review fixes
    
    * feat(checkbox): fix prop type

---
+++ packages/checkbox/src/Component.test.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { Checkbox } from './index';
+
+describe('Checkbox', () => {
+    describe('Display tests', () => {
+        it('should display correctly', () => {
+            expect(render(<Checkbox label='label' hint='hint' />)).toMatchSnapshot();
+        });
+
+        it('should display indeterminate correctly', () => {
+            expect(
+                render(<Checkbox label='label' hint='hint' indeterminate={true} />),
+            ).toMatchSnapshot();
+        });
+    });
+
+    describe('Styles tests', () => {
+        it('should set custom class', () => {
+            const className = 'custom-class';
+            const { container } = render(<Checkbox className={className} />);
+
+            expect(container.firstElementChild).toHaveClass(className);
+        });
+
+        it('should set `checked` class', () => {
+            const { container } = render(<Checkbox checked={true} />);
+
+            expect(container.firstElementChild).toHaveClass('checked');
+        });
+
+        it('should set `disabled` class', () => {
+            const { container } = render(<Checkbox disabled={true} />);
+
+            expect(container.firstElementChild).toHaveClass('disabled');
+        });
+
+        it('should set `indeterminate` class', () => {
+            const { container } = render(<Checkbox indeterminate={true} />);
+
+            expect(container.firstElementChild).toHaveClass('indeterminate');
+        });
+    });
+
+    describe('Attributes tests', () => {
+        it('should set `data-test-id` atribute', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Checkbox dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId).tagName).toBe('INPUT');
+        });
+
+        it('should set disabled attribute', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Checkbox disabled={true} dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId)).toBeDisabled();
+        });
+
+        it('should set checked attribute', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Checkbox checked={true} dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId)).toBeChecked();
+        });
+    });
+
+    describe('Render tests', () => {
+        test('should unmount without errors', () => {
+            const { unmount } = render(<Checkbox />);
+
+            expect(unmount).not.toThrowError();
+        });
+    });
+
+    describe('Interaction tests', () => {
+        test('should call `onChange` prop if not disabled', () => {
+            const cb = jest.fn();
+
+            const { container } = render(<Checkbox onChange={cb} checked={true} />);
+
+            if (container.firstElementChild) {
+                fireEvent.click(container.firstElementChild);
+            }
+
+            expect(cb).toBeCalledTimes(1);
+        });
+
+        test('should not call `onChange` prop if disabled', () => {
+            const cb = jest.fn();
+
+            const { container } = render(<Checkbox onChange={cb} disabled={true} />);
+
+            if (container.firstElementChild) {
+                fireEvent.click(container.firstElementChild);
+            }
+
+            expect(cb).not.toBeCalled();
+        });
+
+        test('should not call `onChange` prop if disabled and checked', () => {
+            const cb = jest.fn();
+
+            const { container } = render(<Checkbox onChange={cb} checked={true} disabled={true} />);
+
+            if (container.firstElementChild) {
+                fireEvent.click(container.firstElementChild);
+            }
+
+            expect(cb).not.toBeCalled();
+        });
+    });
+});
