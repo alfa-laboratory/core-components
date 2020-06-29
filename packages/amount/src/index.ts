702da762cbdd51ab2d4b78d9559b9aba4c15d77e
stepancar <stepancar@hotmail.com>
2020-06-29 14:23:44 +0300

    refactor(core-components-amount): fix exports

--- packages/amount/src/index.ts
+++ packages/amount/src/index.ts
@@ -2,8 +2,7 @@ import { Amount as DefaultAmount } from './component';
 import { PureAmount } from './pure/component';
 
 type AmountType = typeof DefaultAmount & { Pure: typeof PureAmount };
-const Amount: AmountType = DefaultAmount as AmountType;
+export const Amount: AmountType = DefaultAmount as AmountType;
 Amount.Pure = PureAmount;
 
 export * from './types';
-export { Amount };
