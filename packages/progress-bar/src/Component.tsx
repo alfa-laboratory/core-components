74ab6bae3b9ff2d07d0944193712803b2ddd56f6
Roman Chistyakov <rchistyakov@alfabank.ru>
2020-07-14 11:27:09 +0300

    refactor(core-components-progress-bar): small refactor

--- packages/progress-bar/src/Component.tsx
+++ packages/progress-bar/src/Component.tsx
@@ -4,33 +4,42 @@ import cn from 'classnames';
 import styles from './index.module.css';
 
 export type ProgressBarProps = {
-    /** Значение заполненной части 0-100 */
+    /**
+     * Значение заполненной части 0-100
+     */
     value: number;
-    /** Css-класс для стилизации */
+
+    /**
+     * Css-класс для стилизации
+     */
     className?: string;
-    /** Цвет заполнения */
+
+    /**
+     * Цвет заполнения
+     */
     view?: 'positive' | 'negative';
-    /** Id компонента для тестов */
+
+    /**
+     * Id компонента для тестов
+     */
     dataTestId?: string;
 };
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({
-    className,
-    value,
-    view = 'positive',
-    dataTestId,
-}) => (
-    <div
-        role='progressbar'
-        aria-valuenow={Math.round(value)}
-        aria-valuemin={0}
-        aria-valuemax={100}
-        className={cn(styles.container, className)}
-        data-test-id={dataTestId}
-    >
+export const ProgressBar = React.forwardRef<HTMLDivElement, ProgressBarProps>(
+    ({ className, value, view = 'positive', dataTestId }, ref) => (
         <div
-            className={cn(styles.filled, styles[view])}
-            style={{ transform: `translateX(${value - 100}%)` }}
-        />
-    </div>
+            role='progressbar'
+            aria-valuenow={Math.round(value)}
+            aria-valuemin={0}
+            aria-valuemax={100}
+            className={cn(styles.container, className)}
+            data-test-id={dataTestId}
+            ref={ref}
+        >
+            <div
+                className={cn(styles.filled, styles[view])}
+                style={{ transform: `translateX(${value - 100}%)` }}
+            />
+        </div>
+    ),
 );
