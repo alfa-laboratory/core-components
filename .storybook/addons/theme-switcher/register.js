7a745d05c18fe90e5e7e7773f7d7b26cba4c33e4
reme3d2y <AYatsenko@alfabank.ru>
2020-07-15 11:19:29 +0300

    feat: 🎸 add theme switcher to storybook

---
+++ .storybook/addons/theme-switcher/register.js
@@ -0,0 +1,43 @@
+import React, { useCallback, useState } from 'react';
+import addons, { types } from '@storybook/addons';
+import { useChannel } from '@storybook/api';
+import { STORY_RENDERED } from '@storybook/core-events';
+import { Form } from '@storybook/components';
+
+import './index.css';
+
+export const ADDON_ID = 'theme-switcher';
+export const THEMES = ['default', 'click'];
+
+const Addon = () => {
+    const [theme, setTheme] = useState('default');
+
+    const emit = useChannel({
+        [STORY_RENDERED]: () => emit(`${ADDON_ID}/theme`, theme),
+    });
+
+    const handleChange = useCallback(event => {
+        setTheme(event.target.value);
+        emit(`${ADDON_ID}/theme`, event.target.value);
+    }, []);
+
+    return (
+        <div className='tool'>
+            <Form.Select size={1} onChange={handleChange} className='select'>
+                {THEMES.map(themeName => (
+                    <option value={themeName} key={themeName}>
+                        Тема: {themeName}
+                    </option>
+                ))}
+            </Form.Select>
+        </div>
+    );
+};
+
+addons.register(ADDON_ID, () => {
+    addons.add(ADDON_ID, {
+        type: types.TOOL,
+        match: ({ viewMode }) => viewMode === 'story',
+        render: () => <Addon />,
+    });
+});
