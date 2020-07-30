d9d2da6b138eecddf7d5c33f865bd0895de16b4e
reme3d2y <AYatsenko@alfabank.ru>
2020-07-30 11:14:59 +0300

    refactor(core-components-bank-card): add aspect ratio. change styles

--- packages/bank-card/src/Component.tsx
+++ packages/bank-card/src/Component.tsx
@@ -5,7 +5,6 @@ import {
     BankAlfaLBlackIcon,
     CardVisaXxlBlackIcon,
     CardMastercardXxlBlackIcon,
-    PhotoMBlackIcon,
 } from '@alfalab/icons-classic';
 
 import styles from './index.module.css';
@@ -65,6 +64,18 @@ const getBrandIcon = (value = '') => {
     return null;
 };
 
+// TODO: затащить в icons
+const CameraMBlackIcon = () => (
+    <svg width='24' height='24' viewBox='0 0 24 24' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <path
+            fillRule='evenodd'
+            clipRule='evenodd'
+            d='M7.77208 4.68377C7.90819 4.27543 8.29033 4 8.72076 4H15.2792C15.7097 4 16.0918 4.27543 16.2279 4.68377L17 7H20C20.5523 7 21 7.44772 21 8V18C21 18.5523 20.5523 19 20 19H4C3.44772 19 3 18.5523 3 18V8C3 7.44772 3.44772 7 4 7H7L7.77208 4.68377ZM12 16C13.6569 16 15 14.6569 15 13C15 11.3431 13.6569 10 12 10C10.3431 10 9 11.3431 9 13C9 14.6569 10.3431 16 12 16Z'
+            fill='black'
+        />
+    </svg>
+);
+
 /**
  * TODO:
  * 1. Иконки фото нет в наборах
@@ -87,6 +98,9 @@ export const BankCard = React.forwardRef<HTMLInputElement, BankCardProps>(
         },
         ref,
     ) => {
+        const [focused, setFocused] = useState(false);
+        const [filled, setFilled] = useState(value !== undefined && value !== '');
+
         const [brandIcon, setBrandIcon] = useState<ReactNode>(getBrandIcon(value));
 
         const getMask = (newValue: string) =>
@@ -94,6 +108,8 @@ export const BankCard = React.forwardRef<HTMLInputElement, BankCardProps>(
 
         const handleInputChange = useCallback(
             (event: ChangeEvent<HTMLInputElement>) => {
+                setFilled(event.target.value !== '');
+
                 setBrandIcon(getBrandIcon(event.target.value));
 
                 if (onChange) {
@@ -103,32 +119,50 @@ export const BankCard = React.forwardRef<HTMLInputElement, BankCardProps>(
             [onChange],
         );
 
+        const handleInputFocus = useCallback(() => {
+            setFocused(true);
+        }, []);
+
+        const handleInputBlur = useCallback(() => {
+            setFocused(false);
+        }, []);
+
         const renderRightAddons = useCallback(() => {
             return (
                 <button type='button' className={styles.usePhoto} onClick={onUsePhoto}>
-                    <PhotoMBlackIcon />
+                    <CameraMBlackIcon />
                 </button>
             );
         }, [onUsePhoto]);
 
         return (
-            <div className={cn(styles.component, className)} style={{ backgroundColor }}>
-                <div className={styles.bankLogo}>{bankLogo}</div>
+            <div
+                className={cn(styles.component, className, {
+                    [styles.focused]: focused,
+                    [styles.filled]: filled,
+                })}
+                style={{ backgroundColor }}
+            >
+                <div className={styles.container}>
+                    <div className={styles.bankLogo}>{bankLogo}</div>
 
-                <MaskedInput
-                    ref={ref}
-                    value={value}
-                    mask={getMask}
-                    block={true}
-                    label={inputLabel}
-                    rightAddons={renderRightAddons()}
-                    inputClassName={styles.input}
-                    labelClassName={styles.label}
-                    onChange={handleInputChange}
-                    dataTestId={dataTestId}
-                />
+                    <MaskedInput
+                        ref={ref}
+                        value={value}
+                        mask={getMask}
+                        block={true}
+                        label={inputLabel}
+                        rightAddons={renderRightAddons()}
+                        inputClassName={styles.input}
+                        labelClassName={styles.label}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                        onBlur={handleInputBlur}
+                        dataTestId={dataTestId}
+                    />
 
-                {brandIcon && <div className={styles.brandLogo}>{brandIcon}</div>}
+                    {brandIcon && <div className={styles.brandLogo}>{brandIcon}</div>}
+                </div>
             </div>
         );
     },
