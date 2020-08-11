517953e2a46592813b8980472e0eaf49cbf4a213
Кононенко Артем Игоревич <AIKononenko@alfabank.ru>
2020-08-11 11:05:11 +0300

    feat(core-components-textarea): add Textarea component
    #217

---
+++ packages/textarea/src/Component.test.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { Textarea } from './index';
+
+describe('Textarea', () => {
+    describe('Snapshots tests', () => {
+        it('should match snapshot', () => {
+            expect(render(<Textarea value='value' onChange={jest.fn()} />)).toMatchSnapshot();
+        });
+    });
+
+    it('should forward ref to textarea', () => {
+        const textareaRef = jest.fn();
+        const dataTestId = 'test-id';
+        const { getByTestId } = render(<Textarea ref={textareaRef} dataTestId={dataTestId} />);
+
+        expect(textareaRef.mock.calls).toEqual([[getByTestId(dataTestId)]]);
+    });
+
+    it('should set `data-test-id` atribute to textarea', () => {
+        const dataTestId = 'test-id';
+        const { getByTestId } = render(<Textarea block={true} dataTestId={dataTestId} />);
+
+        expect(getByTestId(dataTestId).tagName).toBe('TEXTAREA');
+    });
+
+    describe('Classes tests', () => {
+        it('should set `className` class to root', () => {
+            const className = 'test-class';
+            const { container } = render(<Textarea className={className} />);
+
+            expect(container.firstElementChild).toHaveClass(className);
+        });
+
+        it('should set `textareaClassName` class to textarea', () => {
+            const dataTestId = 'test-id';
+            const className = 'test-class';
+            const { getByTestId } = render(
+                <Textarea textareaClassName={className} dataTestId={dataTestId} />,
+            );
+
+            expect(getByTestId(dataTestId)).toHaveClass(className);
+        });
+
+        it('should set `filled` class', () => {
+            const { container } = render(<Textarea value='some value' readOnly={true} />);
+
+            expect(container.firstElementChild).toHaveClass('filled');
+        });
+
+        it('should set `hasLabel` class', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Textarea label='label' dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId)).toHaveClass('hasLabel');
+        });
+
+        it('should set `disabled` atribute', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Textarea disabled={true} dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId)).toHaveAttribute('disabled');
+        });
+    });
+
+    describe('Callbacks tests', () => {
+        it('should call `onChange` prop', () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const value = '123';
+            const { getByTestId } = render(<Textarea onChange={cb} dataTestId={dataTestId} />);
+
+            const textarea = getByTestId(dataTestId) as HTMLTextAreaElement;
+
+            fireEvent.change(textarea, { target: { value } });
+
+            expect(cb).toBeCalledTimes(1);
+            expect(textarea.value).toBe(value);
+        });
+
+        it('should call `onFocus` prop', () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Textarea onFocus={cb} dataTestId={dataTestId} />);
+
+            fireEvent.focus(getByTestId(dataTestId));
+
+            expect(cb).toBeCalledTimes(1);
+        });
+
+        it('should call `onBlur` prop', () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<Textarea onBlur={cb} dataTestId={dataTestId} />);
+
+            fireEvent.blur(getByTestId(dataTestId));
+
+            expect(cb).toBeCalledTimes(1);
+        });
+
+        it('should not call `onChange` prop if disabled', async () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <Textarea onChange={cb} dataTestId={dataTestId} disabled={true} />,
+            );
+
+            const textarea = getByTestId(dataTestId) as HTMLTextAreaElement;
+
+            await userEvent.type(textarea, '123');
+
+            expect(cb).not.toBeCalled();
+        });
+    });
+
+    it('should unmount without errors', () => {
+        const { unmount } = render(<Textarea value='value' onChange={jest.fn()} />);
+
+        expect(unmount).not.toThrowError();
+    });
+});
---
+++ packages/textarea/src/Component.tsx
@@ -0,0 +1,240 @@
+import React, { useState, useCallback, useEffect, useRef, TextareaHTMLAttributes } from 'react';
+import cn from 'classnames';
+import TextareaAutosize from 'react-textarea-autosize';
+import mergeRefs from 'react-merge-refs';
+import { FormControl } from '@alfalab/core-components-form-control';
+
+import styles from './index.module.css';
+
+export type TextareaProps = Omit<TextareaHTMLAttributes<HTMLTextAreaElement>, 'size' | 'style'> & {
+    /**
+     * Растягивает компонент на ширину контейнера
+     */
+    block?: boolean;
+
+    /**
+     * Размер компонента
+     */
+    size?: 's' | 'm' | 'l';
+
+    /**
+     * Отображение ошибки
+     */
+    error?: string | boolean;
+
+    /**
+     * Текст подсказки
+     */
+    hint?: string;
+
+    /**
+     * Лейбл компонента
+     */
+    label?: React.ReactNode;
+
+    /**
+     * Слот слева
+     */
+    leftAddons?: React.ReactNode;
+
+    /**
+     * Слот справа
+     */
+    rightAddons?: React.ReactNode;
+
+    /**
+     * Слот под компонентом
+     */
+    bottomAddons?: React.ReactNode;
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Дополнительный класс textarea
+     */
+    textareaClassName?: string;
+
+    /**
+     * Управление возможностью подстраивать высоту компонента под высоту текста
+     */
+    autosize?: boolean;
+
+    /**
+     * Управление автозаполнением компонента
+     */
+    autocomplete?: boolean;
+
+    /**
+     * Максимальное количество отображаемых строк (работает только вместе с autosize)
+     */
+    maxRows?: number;
+
+    /**
+     * Минимальное количество отображаемых строк (работает только вместе c autosize)
+     */
+    minRows?: number;
+
+    /**
+     * Максимальная высота элемента
+     */
+    maxHeight?: number;
+
+    /**
+     * Управление возможностью изменения размеров компонента
+     */
+    resize?: 'vertical' | 'none';
+
+    /**
+     * Обработчик события изменения высоты компонента (работает только вместе c autosize)
+     */
+    onHeightChange?: (height?: number) => void;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+};
+
+export const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
+    (
+        {
+            autocomplete = true,
+            autosize = true,
+            size = 's',
+            block = false,
+            bottomAddons,
+            className,
+            dataTestId,
+            disabled,
+            error,
+            hint,
+            textareaClassName,
+            label,
+            leftAddons,
+            onFocus,
+            onBlur,
+            onChange,
+            onHeightChange,
+            rightAddons,
+            maxRows,
+            minRows,
+            maxHeight,
+            resize = 'none',
+            value,
+            rows = 1,
+            ...restProps
+        },
+        ref,
+    ) => {
+        const [focused, setFocused] = useState(false);
+        const [filled, setFilled] = useState(value !== undefined && value !== '');
+
+        const innerRef = useRef<HTMLTextAreaElement>(null);
+
+        // Хак, так как react-textarea-autosize перестал поддерживать maxHeight
+        useEffect(() => {
+            if (autosize && maxHeight && innerRef.current && innerRef.current.style) {
+                innerRef.current.style.maxHeight = `${maxHeight}px`;
+            }
+        }, [innerRef, autosize, maxHeight]);
+
+        const handleTextareaFocus = useCallback(
+            (e: React.FocusEvent<HTMLTextAreaElement>) => {
+                setFocused(true);
+
+                if (onFocus) {
+                    onFocus(e);
+                }
+            },
+            [onFocus],
+        );
+
+        const handleTextareaBlur = useCallback(
+            (e: React.FocusEvent<HTMLTextAreaElement>) => {
+                setFocused(false);
+
+                if (onBlur) {
+                    onBlur(e);
+                }
+            },
+            [onBlur],
+        );
+
+        const handleTextareaChange = useCallback(
+            (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+                setFilled(e.target.value !== '');
+
+                if (onChange) {
+                    onChange(e);
+                }
+            },
+            [onChange],
+        );
+
+        const textareaProps = {
+            ...restProps,
+            className: cn(
+                styles.textarea,
+                styles[size],
+                {
+                    [styles.hasLabel]: label,
+                    [styles.filled]: filled,
+                    [styles.resizeVertical]: resize === 'vertical',
+                },
+                textareaClassName,
+            ),
+            autoComplete: autocomplete === false ? 'off' : 'on',
+            disabled,
+            onBlur: handleTextareaBlur,
+            onFocus: handleTextareaFocus,
+            onChange: handleTextareaChange,
+            rows,
+            'data-test-id': dataTestId,
+        };
+
+        return (
+            <FormControl
+                className={className}
+                size={size}
+                block={block}
+                disabled={disabled}
+                filled={filled || focused || !!value}
+                focused={focused}
+                error={error}
+                label={label}
+                labelClassName={cn(styles.label, styles[size])}
+                hint={hint}
+                leftAddons={leftAddons}
+                rightAddons={rightAddons}
+                bottomAddons={bottomAddons}
+            >
+                {autosize ? (
+                    <TextareaAutosize
+                        {...textareaProps}
+                        maxRows={maxRows}
+                        minRows={minRows}
+                        onHeightChange={onHeightChange}
+                        value={value}
+                        ref={mergeRefs([ref, innerRef])}
+                    />
+                ) : (
+                    <textarea {...textareaProps} style={{ maxHeight }} value={value} ref={ref} />
+                )}
+            </FormControl>
+        );
+    },
+);
+
+/**
+ * Для отображения в сторибуке
+ */
+Textarea.defaultProps = {
+    autocomplete: true,
+    autosize: true,
+    size: 's',
+    block: false,
+    resize: 'none',
+};
