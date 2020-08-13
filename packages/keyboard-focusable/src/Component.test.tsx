95201976e5e64943aebb5b58adfb728aa7ec2864
reme3d2y <AYatsenko@alfabank.ru>
2020-08-13 12:54:43 +0300

    feat(core-components-keyboard-focusable): use hook, refactor

--- packages/keyboard-focusable/src/Component.test.tsx
+++ packages/keyboard-focusable/src/Component.test.tsx
@@ -1,15 +1,19 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
-import { KeyboardFocusable, useKeyboardFocus } from './index';
+import { KeyboardFocusable } from './index';
 
 describe('KeyboardFocusable', () => {
     const renderComponent = () =>
         render(
             <React.Fragment>
                 <KeyboardFocusable>
-                    {focused => <button type='button'>{focused ? 'focused' : ''}</button>}
+                    {(ref, focused) => (
+                        <button type='button' ref={ref}>
+                            {focused ? 'focused' : ''}
+                        </button>
+                    )}
                 </KeyboardFocusable>
                 <button type='button'>second button</button>
             </React.Fragment>,
@@ -50,82 +54,9 @@ describe('KeyboardFocusable', () => {
     it('should pass `focused=false` for non-interactive child', async () => {
         const { queryByText } = render(
             <KeyboardFocusable>
-                {focused => <div>{focused ? 'focused' : ''}</div>}
+                {(ref, focused) => <div ref={ref}>{focused ? 'focused' : ''}</div>}
             </KeyboardFocusable>,
         );
-
-        userEvent.tab();
-
-        expect(queryByText('focused')).not.toBeInTheDocument();
-    });
-
-    it('should pass `focused=false` when child gets focus by mouse', async () => {
-        const { queryByText, queryAllByRole } = renderComponent();
-
-        const focusableButton = queryAllByRole('button')[0];
-
-        userEvent.click(focusableButton);
-
-        expect(queryByText('focused')).not.toBeInTheDocument();
-    });
-});
-
-describe('useKeyboardFocus', () => {
-    const renderComponent = () => {
-        const FocusableButton = () => {
-            const ref = useRef<HTMLButtonElement>(null);
-            const { focused } = useKeyboardFocus(ref);
-
-            return (
-                <button type='button' ref={ref}>
-                    {focused ? 'focused' : ''}
-                </button>
-            );
-        };
-
-        return render(
-            <React.Fragment>
-                <FocusableButton />
-                <button type='button'>second button</button>
-            </React.Fragment>,
-        );
-    };
-
-    it('should pass `focused=false` by default', async () => {
-        const { queryByText } = renderComponent();
-
-        expect(queryByText('focused')).not.toBeInTheDocument();
-    });
-
-    it('should pass `focused=true` when child gets focus', async () => {
-        const { queryByText } = renderComponent();
-
-        userEvent.tab();
-
-        expect(queryByText('focused')).toBeInTheDocument();
-    });
-
-    it('should pass `focused=false` when child loses focus', async () => {
-        const { queryByText } = renderComponent();
-
-        userEvent.tab();
-
-        expect(queryByText('focused')).toBeInTheDocument();
-
-        userEvent.tab();
-
-        expect(queryByText('focused')).not.toBeInTheDocument();
-    });
-
-    it('should pass `focused=false` for non-interactive ref element', async () => {
-        const FocusableButton = () => {
-            const ref = useRef<HTMLDivElement>(null);
-            const { focused } = useKeyboardFocus(ref);
-
-            return <div ref={ref}>{focused ? 'focused' : ''}</div>;
-        };
-
-        const { queryByText } = render(<FocusableButton />);
 
         userEvent.tab();
 
--- packages/keyboard-focusable/src/Component.tsx
+++ packages/keyboard-focusable/src/Component.tsx
@@ -1,79 +1,21 @@
-import React, { useState, useEffect, useCallback, RefObject, ReactNode, useRef } from 'react';
-import NodeResolver from 'react-node-resolver';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useRef, RefObject } from 'react';
 
-let prevInputMethod: 'mouse' | 'keyboard';
-
-const handleKeyDown = () => {
-    prevInputMethod = 'keyboard';
-};
-
-const handleMouseDown = () => {
-    prevInputMethod = 'mouse';
-};
-
-/**
- * Навешивает несколько глобальных обработчиков и отслеживает метод ввода - мышь или клавиатура.
- * Note: Повторный вызов функции не дублирует обработчики
- */
-function addGlobalListeners() {
-    document.addEventListener('keydown', handleKeyDown);
-    document.addEventListener('mousedown', handleMouseDown);
-    document.addEventListener('touchstart', handleMouseDown);
-}
-
-export function useKeyboardFocus(ref: RefObject<HTMLElement>) {
-    const [focused, setFocused] = useState(false);
-
-    const handleFocus = useCallback(() => {
-        if (prevInputMethod === 'keyboard') {
-            setFocused(true);
-        }
-    }, []);
-
-    const handleBlur = useCallback(() => {
-        setFocused(false);
-    }, []);
-
-    useEffect(() => {
-        const node = ref.current;
-
-        if (node) {
-            node.addEventListener('focusin', handleFocus);
-            node.addEventListener('focusout', handleBlur);
-        }
-
-        return () => {
-            if (node) {
-                node.removeEventListener('focusin', handleFocus);
-                node.removeEventListener('focusout', handleBlur);
-            }
-        };
-    }, [handleBlur, handleFocus, ref]);
-
-    useEffect(() => {
-        addGlobalListeners();
-    }, []);
-
-    return {
-        focused,
-    };
-}
+import { useFocus } from '@alfalab/hooks';
 
 type KeyboardFocusableProps = {
     /**
-     * Рендер-проп, в который передается состояние фокуса
+     * Рендер-проп, в который передается состояние фокуса и реф.
+     *
+     * Реф нужно установить на интерактивный элемент или на одного из его родителей.
      */
-    children?: (focused: boolean) => ReactNode;
+    children: (ref: RefObject<any>, focused: boolean) => JSX.Element;
 };
 
 export const KeyboardFocusable = ({ children }: KeyboardFocusableProps) => {
     const targetRef = useRef<HTMLElement | null>(null);
 
-    const { focused } = useKeyboardFocus(targetRef);
+    const [focused] = useFocus('keyboard', targetRef);
 
-    const handleTargetRef = useCallback(node => {
-        targetRef.current = node;
-    }, []);
-
-    return <NodeResolver innerRef={handleTargetRef}>{children && children(focused)}</NodeResolver>;
+    return children(targetRef, focused);
 };
