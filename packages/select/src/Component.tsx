1e1bfdcdbdfc49b4aff84da49de96c638436c78c
Alex Yatsenko <reme3d2y@gmail.com>
2020-08-17 15:44:01 +0300

    Refactor/select (#204)
    * refactor(core-components-select): useCombobox & refactor
    
    * refactor(core-components-select): small fixes
    
    * refactor(core-components-select): change option-shape
    
    * test(core-components-select): add tests to joinOptions
    
    * chore: update deps

--- packages/select/src/Component.tsx
+++ packages/select/src/Component.tsx
@@ -1,114 +1,29 @@
-import React, { useCallback, ReactNode, ComponentType } from 'react';
-import cn from 'classnames';
-import { Field as DefaultField, FieldProps } from './components/field';
+import React from 'react';
+import { Field as DefaultField } from './components/field';
 import { Arrow as DefaultArrow } from './components/arrow';
 import { OptionsList as DefaultOptionsList } from './components/options-list';
-import { Option as DefaultOption, OptionProps } from './components/option';
+import { Option as DefaultOption } from './components/option';
 import { Optgroup as DefaultOptgroup } from './components/optgroup';
 import { BaseSelect } from './components/base-select';
-import { BaseSelectProps, BaseArrowProps } from './typings';
+import { BaseSelectProps } from './typings';
 
-import styles from './index.module.css';
-
-export type SelectProps = Omit<BaseSelectProps, 'Field' | 'OptionsList' | 'Option' | 'Optgroup'> & {
-    /**
-     * Размер компонента
-     */
-    size?: 's' | 'm' | 'l';
-
-    /**
-     * Растягивает компонент на ширину контейнера
-     */
-    block?: boolean;
-
-    /**
-     * Лейбл поля
-     */
-    label?: ReactNode;
-
-    /**
-     * Плейсхолдер поля
-     */
-    placeholder?: string;
-
-    /**
-     * Компонент поля
-     */
-    Arrow?: ComponentType<BaseArrowProps>;
-
-    /**
-     * Компонент поля
-     */
-    Field?: BaseSelectProps['Field'];
-
-    /**
-     * Компонент выпадающего меню
-     */
-    OptionsList?: BaseSelectProps['OptionsList'];
-
-    /**
-     * Компонент группы
-     */
-    Optgroup?: BaseSelectProps['Optgroup'];
-
-    /**
-     * Компонент пункта меню
-     */
-    Option?: BaseSelectProps['Option'];
-
-    /**
-     * Кастомный рендер выбранного пункта
-     */
-    valueRenderer?: FieldProps['valueRenderer'];
-
-    /**
-     * Кастомный рендер пункта меню
-     */
-    optionRenderer?: OptionProps['optionRenderer'];
-};
+export type SelectProps = BaseSelectProps;
 
 export const Select = ({
-    block = false,
-    size = 's',
-    label,
-    placeholder,
-    valueRenderer,
-    optionRenderer,
-    Field = DefaultField,
     Arrow = DefaultArrow,
-    Option = DefaultOption,
+    Field = DefaultField,
     OptionsList = DefaultOptionsList,
     Optgroup = DefaultOptgroup,
+    Option = DefaultOption,
     ...restProps
 }: SelectProps) => {
-    const renderField = useCallback(
-        props => (
-            <Field
-                {...props}
-                valueRenderer={valueRenderer}
-                rightAddons={Arrow && <Arrow open={props.open} />}
-                label={label}
-                placeholder={placeholder}
-                size={size}
-            />
-        ),
-        [Arrow, label, placeholder, size, valueRenderer],
-    );
-
-    const renderOption = useCallback(
-        props => <Option {...props} optionRenderer={optionRenderer} size={size} />,
-        [optionRenderer, size],
-    );
-
-    const renderOptionsList = useCallback(props => <OptionsList {...props} size={size} />, [size]);
-
     return (
         <BaseSelect
-            className={cn({ [styles.block]: block })}
-            Field={renderField}
-            Option={renderOption}
-            OptionsList={renderOptionsList}
+            Option={Option}
+            Field={Field}
             Optgroup={Optgroup}
+            OptionsList={OptionsList}
+            Arrow={Arrow}
             {...restProps}
         />
     );
