2fb939ebd302308697477597f3b59ca63339ab9e
reme3d2y <AYatsenko@alfabank.ru>
2020-09-25 11:36:45 +0300

    feat: update docs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -120,10 +120,6 @@ module.exports = ({ config }) => ({
                 withRDTL: process.env.RDTL !== 'off',
             }),
             {
-                test: /\.(jpg|png)$/,
-                use: 'file-loader'
-            },
-            {
                 test: /\.js$/,
                 use: [
                     {
