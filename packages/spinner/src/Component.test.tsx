278df79f7316f8ae78996d4c1bd578f786f7a4ad
opshilov <opshilov@alfabank.ru>
2020-11-16 18:01:15 +0300

    feat(core-components-spinner): add spinner component

---
+++ packages/spinner/src/Component.test.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Spinner } from './index';
+
+const testId = 'spinner';
+
+describe('Snapshots tests', () => {
+    it('should display correctly', () => {
+        expect(render(<Spinner />)).toMatchSnapshot();
+    });
+});
+
+describe('Attributes tests', () => {
+    it('should set data-test-id attribute', async () => {
+        const className = 'custom';
+
+        render(<Spinner className={className} dataTestId={testId} />);
+
+        const spinnerContentWrap = document.querySelector(`.${className}`);
+
+        const testIdAttr = spinnerContentWrap?.getAttribute('data-test-id');
+
+        expect(testIdAttr).toBe(testId);
+    });
+});
+
+describe('Render tests', () => {
+    it('should unmount without errors', async () => {
+        const { unmount } = render(<Spinner />);
+
+        expect(unmount).not.toThrowError();
+    });
+
+    it('should have visible class if prop visible is true', async () => {
+        const { getByTestId } = render(<Spinner dataTestId={testId} visible={true} />);
+
+        expect(getByTestId(testId)).toHaveClass('visible');
+    });
+});
---
+++ packages/spinner/src/Component.tsx
@@ -0,0 +1,42 @@
+import React, { FC } from 'react';
+import cn from 'classnames';
+import styles from './Spinner.css';
+
+export type SpinnerProps = {
+    /**
+     * Управление видимостью компонента
+     */
+    visible?: boolean;
+
+    /**
+     * Размер компонента
+     */
+    size?: 's' | 'm';
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Идентификатор компонента в DOM
+     */
+    id?: string;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+};
+
+export const Spinner: FC<SpinnerProps> = ({ size = 's', visible, id, className, dataTestId }) => {
+    return (
+        <span
+            className={cn(className, styles.spinner, styles[`size-${size}`], {
+                [styles.visible]: visible,
+            })}
+            id={id}
+            data-test-id={dataTestId}
+        />
+    );
+};
