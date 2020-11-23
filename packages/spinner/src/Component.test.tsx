8c6e45341b0ac6232e2fc89ed6860bb8ccbc371c
sitesfaction <nikas64527@gmail.com>
2020-11-23 17:30:29 +0300

    feat(core-components-spinner): add spinner component (#360)
    * feat(core-components-spinner): add spinner component
    
    * refactor(core-components-spinner): review fixes
    
    * refactor(core-components-spinner): remove exceed icons
    
    * refactor(core-components-spinner): review fixes
    
    * fix(core-components-spinner): move svg from component to css base 64
    
    Co-authored-by: opshilov <opshilov@alfabank.ru>

---
+++ packages/spinner/src/Component.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { Spinner } from './index';
+
+const testId = 'spinner';
+
+describe('Snapshots tests', () => {
+    it('should display correctly', () => {
+        const { container } = render(<Spinner />);
+
+        expect(container).toMatchSnapshot();
+    });
+});
+
+describe('Attributes tests', () => {
+    it('should set data-test-id attribute', async () => {
+        const className = 'custom';
+
+        render(<Spinner className={className} dataTestId={testId} />);
+
+        const spinnerContentWrap = document.querySelector(`.${className}`);
+
+        const testIdAttr = spinnerContentWrap?.getAttribute('data-test-id');
+
+        expect(testIdAttr).toBe(testId);
+    });
+});
+
+describe('Render tests', () => {
+    it('should unmount without errors', async () => {
+        const { unmount } = render(<Spinner />);
+
+        expect(unmount).not.toThrowError();
+    });
+
+    it('should have visible class if prop visible is true', async () => {
+        const { getByTestId } = render(<Spinner dataTestId={testId} visible={true} />);
+
+        expect(getByTestId(testId)).toHaveClass('visible');
+    });
+
+    it('should set `size` class', () => {
+        const size = 's';
+        const { container } = render(<Spinner visible={true} size={size} />);
+
+        expect(container.firstElementChild).toHaveClass(size);
+    });
+
+    it('should use default `size`', () => {
+        const { container } = render(<Spinner visible={true} />);
+
+        expect(container.firstElementChild).toHaveClass('s');
+    });
+});
