945a45635e6a90f295c27a61dac9ec08676669c3
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2020-12-10 20:53:27 +0300

    feat(space): just add Space component

---
+++ packages/space/src/Component.tsx
@@ -0,0 +1,164 @@
+import React, { ReactNode, Children, forwardRef } from 'react';
+import classNames from 'classnames';
+import Item from './Item';
+import styles from './index.module.css';
+
+export type Align = 'start' | 'end' | 'center';
+export type Direction = 'horizontal' | 'vertical';
+export type Size = 's' | 'm' | 'l' | number;
+
+export const SpaceContext = React.createContext({
+    length: 0,
+    horizontalSize: 0,
+    verticalSize: 0,
+});
+
+export type SpaceProps = {
+    /**
+     * Заголовок группы
+     */
+    label?: ReactNode;
+
+    /**
+     * Направление
+     */
+    align?: Align;
+
+    /**
+     * Направление
+     */
+    direction?: Direction;
+
+    /**
+     * Тип компонента
+     */
+    size?: Size | [Size, Size];
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Дочерние элементы
+     */
+    children: ReactNode;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+
+    /**
+     * Автоматический перенос строк, полезно при direction = 'horizontal'
+     */
+    wrap?: boolean;
+
+    /**
+     * Компонент разделителя
+     */
+    split?: boolean;
+
+    /**
+     * Растягивать ли компонент на всю ширину
+     */
+    fullWidth?: boolean;
+};
+
+const SpaceSizes: { [key in Size]: number } = {
+    s: 12,
+    m: 16,
+    l: 20,
+};
+
+const getNumberSize = (size: Size) => {
+    return typeof size === 'string' ? SpaceSizes[size] : size || 0;
+};
+
+/**
+ * Позаимствовано с благодарностью из Ant Design
+ */
+
+export const Space = forwardRef<HTMLDivElement, SpaceProps>((props, ref) => {
+    const {
+        children,
+        className,
+        label,
+        align = 'start',
+        direction = 'vertical',
+        size = 'm',
+        wrap = false,
+        split = false,
+        fullWidth = false,
+    } = props;
+
+    const [horizontalSize, verticalSize] = React.useMemo(
+        () =>
+            ((Array.isArray(size) ? size : [size, size]) as [Size, Size]).map(item =>
+                getNumberSize(item),
+            ),
+        [size],
+    );
+
+    const childNodes = Children.toArray(children);
+
+    if (childNodes.length === 0) {
+        return null;
+    }
+
+    const directionCls = styles[direction];
+
+    const alignCls = styles[`align_${align}`];
+
+    const containerCls = classNames(
+        styles.spaceContainer,
+        directionCls,
+        {
+            [alignCls]: align,
+            [styles.spaceContainerFullWidth]: fullWidth,
+        },
+        className,
+    );
+
+    const itemCls = classNames(styles.spaceItem, {
+        [styles.spaceItemFullWidth]: fullWidth,
+    });
+
+    const nodes = childNodes.map((child, i) => (
+        /* eslint-disable react/no-array-index-key */
+        <Item
+            className={itemCls}
+            key={`${itemCls}-${i}`}
+            direction={direction}
+            index={i}
+            wrap={wrap}
+            split={split}
+        >
+            {child}
+        </Item>
+        /* eslint-enable */
+    ));
+
+    const componentCls = classNames({
+        [styles.spaceFullWidth]: fullWidth,
+    });
+
+    return (
+        <div className={componentCls}>
+            {label ? <span className={styles.label}>{label}</span> : null}
+            <div
+                className={containerCls}
+                style={{
+                    ...(wrap && { flexWrap: 'wrap', marginBottom: -verticalSize }),
+                }}
+                ref={ref}
+            >
+                <SpaceContext.Provider
+                    value={{ horizontalSize, verticalSize, length: childNodes.length }}
+                >
+                    {nodes}
+                </SpaceContext.Provider>
+            </div>
+        </div>
+    );
+});
---
+++ packages/space/src/Item.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import { Direction, SpaceContext } from '.';
+
+export interface ItemProps {
+    className: string;
+    children: React.ReactNode;
+    index: number;
+    direction?: Direction;
+    split?: string | React.ReactNode;
+    wrap?: boolean;
+}
+
+const Item = (props: ItemProps) => {
+    const { className, direction, index, children, split, wrap } = props;
+    const { horizontalSize, verticalSize, length } = React.useContext(SpaceContext);
+
+    let style: React.CSSProperties = {};
+
+    if (direction === 'vertical') {
+        if (index < length) {
+            style = { marginBottom: horizontalSize / (split ? 2 : 1) };
+        }
+    } else {
+        style = {
+            ...(index < length && { marginRight: horizontalSize / (split ? 2 : 1) }),
+            ...(wrap && { paddingBottom: verticalSize }),
+        };
+    }
+
+    if (children === null || children === undefined) {
+        return null;
+    }
+
+    return (
+        <React.Fragment>
+            <div className={className} style={style}>
+                {children}
+            </div>
+            {index < length - 1 && split && (
+                <span
+                    style={{
+                        width: '100%',
+                        ...style,
+                    }}
+                >
+                    {split}
+                </span>
+            )}
+        </React.Fragment>
+    );
+};
+
+export default Item;
