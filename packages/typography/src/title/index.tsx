573749f40f8033e14e2fd3b5bb63d0c3346d93d7
reme3d2y <AYatsenko@alfabank.ru>
2020-12-24 09:30:53 +0300

    feat(typography): add default margins

--- packages/typography/src/title/index.tsx
+++ packages/typography/src/title/index.tsx
@@ -2,15 +2,15 @@ import React, { FC } from 'react';
 
 import { Title as TitleBase, TitleProps } from './component';
 
-import styles from './index.module.css';
 import commonStyles from './common.module.css';
+import styles from './index.module.css';
 
 const Title: FC<TitleProps> = props => {
     /**
      * Если поменять Object.assign на деструктуризацию, то упадут тесты.
      * Видимо, это особенность работы jest и css-modules.
      */
-    return <TitleBase {...props} styles={Object.assign(styles, commonStyles)} />;
+    return <TitleBase {...props} styles={Object.assign(commonStyles, styles)} />;
 };
 
 export { Title, TitleProps };
