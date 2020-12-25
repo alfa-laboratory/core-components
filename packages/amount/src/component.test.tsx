d86af5f8a81f3c84201e3462251748f9713c675c
dmitrsavk <dmitrsavk@yandex.ru>
2020-12-25 15:31:45 +0300

    test: fix test

--- packages/amount/src/component.test.tsx
+++ packages/amount/src/component.test.tsx
@@ -4,7 +4,7 @@ import { render } from '@testing-library/react';
 import { Amount } from '.';
 
 describe('Amount', () => {
-    it.skip('should match snapshots for base and Pure components', () => {
+    it('should match snapshots for base and Pure components', () => {
         const { container } = render(
             <React.Fragment>
                 <Amount value={100} currency='RUR' minority={100} />
