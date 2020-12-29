a3818d33667386f28095d994296234b74f72b8af
dmitrsavk <dmitrsavk@yandex.ru>
2020-12-29 15:34:36 +0300

    feat: add input screenshots tests

---
+++ packages/input/src/component.screenshots.test.tsx
@@ -0,0 +1,58 @@
+import { screenshotTesting, getComponentScreenshotTestCases } from '../../utils';
+
+const sizesBlockDisabledCases = getComponentScreenshotTestCases({
+    name: 'input',
+    knobs: {
+        size: ['s', 'm', 'l'],
+        block: [false, true],
+        disabled: [false, true],
+    },
+});
+
+const sizesPlaceholderLabelCases = getComponentScreenshotTestCases({
+    name: 'input',
+    knobs: {
+        size: ['s', 'm', 'l'],
+        placeholder: ['', 'Placeholder'],
+        label: ['', 'Label'],
+        success: [true, false],
+    },
+});
+
+const sizesHintErrorCases = getComponentScreenshotTestCases({
+    name: 'input',
+    knobs: {
+        size: ['s', 'm', 'l'],
+        hint: ['', 'Hint'],
+        error: ['', 'Error'],
+    },
+});
+
+const addonsCases = getComponentScreenshotTestCases({
+    name: 'input',
+    knobs: {
+        rightAddons: [true, false],
+        leftAddons: [true, false],
+        bottomAddons: [true, false],
+    },
+});
+
+describe(
+    'Button | screenshots sizes, blocks and disabled',
+    screenshotTesting(sizesBlockDisabledCases, it, beforeAll, afterAll, expect),
+);
+
+describe(
+    'Button | screenshots size, placeholder and label',
+    screenshotTesting(sizesPlaceholderLabelCases, it, beforeAll, afterAll, expect),
+);
+
+describe(
+    'Button | screenshots size, hint and error',
+    screenshotTesting(sizesHintErrorCases, it, beforeAll, afterAll, expect),
+);
+
+describe(
+    'Button | screenshots addons',
+    screenshotTesting(addonsCases, it, beforeAll, afterAll, expect),
+);
