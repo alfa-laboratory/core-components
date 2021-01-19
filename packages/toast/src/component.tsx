d04fbb315adda99553f87f5d8e477ff60314a070
dmitrsavk <dmitrsavk@yandex.ru>
2021-01-19 18:00:20 +0300

    feat(toast): add draft component

--- packages/toast/src/component.tsx
+++ packages/toast/src/component.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { ToastPlate, ToastPlateProps } from '@alfalab/core-components-toast-plate';
+import { Popover, PopoverProps } from '@alfalab/core-components-popover';
 
-export type ToastProps = {};
+export type ToastProps = {
+    popoverProps: PopoverProps;
+    toastPlateProps: ToastPlateProps;
+};
 
-export const Toast = () => {
-    return <div>Toast</div>;
+export const Toast: FC<ToastProps> = ({ toastPlateProps, popoverProps }) => {
+    return (
+        <Popover {...popoverProps}>
+            <ToastPlate {...toastPlateProps} />
+        </Popover>
+    );
 };
