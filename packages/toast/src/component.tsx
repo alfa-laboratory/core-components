24cc3089a04e7f86ca929192405b71209ed3ebe7
dmitrsavk <dmitrsavk@yandex.ru>
2021-01-22 13:26:47 +0300

    feat(toast): updates

--- packages/toast/src/component.tsx
+++ packages/toast/src/component.tsx
@@ -1,5 +1,7 @@
 import React, { forwardRef, useCallback, useEffect, useRef } from 'react';
 import mergeRefs from 'react-merge-refs';
+import { CSSTransition } from 'react-transition-group';
+import cn from 'classnames';
 import { useClickOutside, usePrevious } from '@alfalab/hooks';
 import { ToastPlate, ToastPlateProps } from '@alfalab/core-components-toast-plate';
 import { Popover, PopoverProps } from '@alfalab/core-components-popover';
@@ -8,7 +10,12 @@ import { Portal } from '@alfalab/core-components-portal';
 import styles from './index.module.css';
 
 export type ToastProps = ToastPlateProps &
-    Pick<PopoverProps, 'anchorElement' | 'position' | 'offset' | 'open' | 'getPortalContainer'> & {
+    Pick<PopoverProps, 'position' | 'offset' | 'open' | 'getPortalContainer'> & {
+        /**
+         * Элемент, относительного которого появляется тост
+         */
+        anchorElement?: HTMLElement;
+
         /**
          * Через сколько исчезнет компонент (ms)
          */
@@ -17,7 +24,12 @@ export type ToastProps = ToastPlateProps &
         /**
          * Обработчик закрытия компонента
          */
-        onClose: () => void;
+        onClose?: () => void;
+
+        /**
+         * Отступ снизу
+         */
+        bottomOffset?: number;
     };
 
 export const Toast = forwardRef<HTMLDivElement, ToastProps>(
@@ -28,6 +40,9 @@ export const Toast = forwardRef<HTMLDivElement, ToastProps>(
             offset,
             open,
             autoCloseDelay = 3000,
+            className,
+            bottomOffset,
+            style = {},
             onClose,
             getPortalContainer,
             ...restProps
@@ -72,7 +87,28 @@ export const Toast = forwardRef<HTMLDivElement, ToastProps>(
 
         return (
             <Portal getPortalContainer={getPortalContainer}>
-                <ToastPlate {...restProps} ref={mergeRefs([ref, plateRef])} onClose={onClose} />
+                <CSSTransition
+                    unmountOnExit={true}
+                    in={open}
+                    timeout={150}
+                    classNames={{
+                        enter: styles.enter,
+                        enterActive: styles.enterActive,
+                        exit: styles.exit,
+                        exitActive: styles.exitActive,
+                    }}
+                >
+                    <ToastPlate
+                        {...restProps}
+                        ref={mergeRefs([ref, plateRef])}
+                        onClose={onClose}
+                        className={cn(styles.fixed, className)}
+                        style={{
+                            ...style,
+                            bottom: bottomOffset && `${bottomOffset}px`,
+                        }}
+                    />
+                </CSSTransition>
             </Portal>
         );
     },
