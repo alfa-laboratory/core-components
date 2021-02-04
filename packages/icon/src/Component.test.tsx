fbe7f527f4c01b6253957640d85d6ea617c8c94a
Olga Bolotova <OBolotova@alfabank.ru>
2021-02-04 10:07:26 +0300

    feat(icon): add component

--- packages/icon/src/Component.test.tsx
+++ packages/icon/src/Component.test.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, waitForElement, act } from '@testing-library/react';
 
+import axiosMock from'axios';
+import axios from'axios';
 import { Icon } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Icon', () => {
     it('should uses a `name` prop and becomes a svg', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: { greeting: 'hello there' } });
         const { container } = render(<Icon name='glyph_debt_m' />);
-
-        await waitFor(() => expect(container.querySelector('svg')).not.toBe(null));
+        const countSpan = await waitFor(() => expect(container.querySelector('svg')).not.toBe(null));
     });
-
+    it('', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: { greeting: 'hello there' } });
+        const { container } = render(<Icon name="fake-fake-fake"/>);
+        const countSpan = await waitFor(() => expect(container.querySelector('svg')).toHaveClass('star'));
+    });
     it('should use a color prop', async () => {
+        mockedAxios.get.mockResolvedValueOnce({ data: { greeting: 'hello there' } });
         const color = '#ccc';
         const { container } = render(<Icon name='glyph_debt_m' color={color} />);
-
-        await waitFor(() => expect(container.firstElementChild).toHaveStyle(`color: ${color}`));
+        const countSpan = await waitFor(() => expect(container.firstElementChild).toHaveStyle(`color: ${color}`));
     });
 });
--- packages/icon/src/Component.tsx
+++ packages/icon/src/Component.tsx
@@ -10,26 +10,36 @@ type IconProps = {
     dataTestId?: string;
 };
 
-export const Icon: React.FC<IconProps> = ({ name, color, dataTestId }) => {
+export const Icon: React.FC<IconProps> = ({
+    name,
+    color,
+    dataTestId,
+}) => {
     const [icon, setIcon] = useState('');
 
     useEffect(() => {
         axios
             .get(`https://alfabank.st/icons/${name}.svg`)
-            .then(res => res.data)
-            .then(svg => {
-                const cleanSvg = DOMPurify.sanitize(svg, {
-                    USE_PROFILES: { svg: true, svgFilters: true },
-                });
+            .then((res) => res.data)
+            .then((svg) => {
+                if (svg.startsWith('<svg')) {
+                    const cleanSvg = DOMPurify.sanitize(svg, {
+                        USE_PROFILES: { svg: true, svgFilters: true },
+                    });
 
-                if (cleanSvg.startsWith('<svg')) {
                     setIcon(cleanSvg);
                 } else {
                     throw new Error();
                 }
             })
             .catch(() => {
-                console.log('error');
+                const fallbackIcon =
+                    '<svg class="star" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><rect width="24" height="24" fill="none"/>\n' +
+                    '<path d="M9.25897 8.13104L12 1V8.40327V17L5 21L7.32079 13.8805L7.41378 13.6046L7.17119 13.425L1 8.5L8.87993 8.40327H9.1672L9.25897 8.13104Z" fill="#0B1F35"/>\n' +
+                    '<path d="M14.741 8.13106L12 1V8.40327V17L19 21L16.6792 13.8805L16.5862 13.6046L16.8288 13.425L23 8.50001L15.1201 8.40328H14.8328L14.741 8.13106Z" fill="#0B1F35"/>\n' +
+                    '</svg>';
+
+                setIcon(fallbackIcon);
             });
     }, [name]);
 
@@ -37,7 +47,7 @@ export const Icon: React.FC<IconProps> = ({ name, color, dataTestId }) => {
         // eslint-disable-next-line react/no-danger
         <span
             style={{ color }}
-            className={styles.iconWrapper}
+            className={styles.component}
             dangerouslySetInnerHTML={{ __html: icon }}
             data-test-id={dataTestId}
         />
