0ff989fa09e482a5e9bda1d64bf5eed9241fc451
Dmitry Savkin <dmitrsavk@yandex.ru>
2021-02-08 19:11:55 +0300

    feat(select-with-tags): design review fixes (#472)
    * feat(select-with-tags): fix gaps
    
    * feat(select-with-tags): add empty list placeholder, fix colors
    
    * feat(select-with-tags): fix list open/close
    
    * feat(select-with-tags): add input on new line
    
    * feat(select-with-tags): fix label
    
    * feat(select-with-tags): add tag render prop
    
    * docs(select-with-tags): updates
    
    * feat(select-with-tags): change renderTag fn to Component
    
    * feat(input-autocomplete): arrow click behaviour
    
    Co-authored-by: reme3d2y <AYatsenko@alfabank.ru>

---
+++ packages/select-with-tags/src/components/tag/component.tsx
@@ -0,0 +1,25 @@
+import React, { useCallback } from 'react';
+import { Tag as CoreTag } from '@alfalab/core-components-tag';
+import { CrossCompactMIcon } from '@alfalab/icons-glyph/CrossCompactMIcon';
+
+import { TagComponent } from '../../types';
+
+import styles from './index.module.css';
+
+export const Tag: TagComponent = ({ option: { content, key }, handleDeleteTag }) => {
+    const handleClick = useCallback(() => {
+        if (handleDeleteTag) {
+            handleDeleteTag(key);
+        }
+    }, [handleDeleteTag, key]);
+
+    return (
+        <CoreTag key={key} size='xs' checked={true} className={styles.tag}>
+            <span className={styles.tagContentWrap}>
+                {content}
+
+                <CrossCompactMIcon onClick={handleClick} className={styles.tagCross} />
+            </span>
+        </CoreTag>
+    );
+};
