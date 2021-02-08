ec95bbc774c83269e64c47e61b8117dae4d65afc
Olga Bolotova <OBolotova@alfabank.ru>
2021-02-08 18:38:32 +0300

    feat(icon): remove axios

--- packages/CDNIcon/src/Component.test.tsx
+++ packages/CDNIcon/src/Component.test.tsx
@@ -1,21 +1,15 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 
-import axios from 'axios';
 import { CDNIcon } from './index';
-
-jest.mock('axios');
-const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('CDNIcon', () => {
     it('should use the `name` prop and becomes a svg', async () => {
-        mockedAxios.get.mockResolvedValueOnce({ data: '<svg />' });
         const { container } = render(<CDNIcon name='glyph_debt_m' />);
         await waitFor(() => expect(container.querySelector('svg')).not.toBe(null));
     });
 
     it('should use the color prop', async () => {
-        mockedAxios.get.mockResolvedValueOnce({ data: '<svg />' });
         const color = '#ccc';
         const { container } = render(<CDNIcon name='glyph_debt_m' color={color} />);
         await waitFor(() => expect(container.firstElementChild).toHaveStyle(`color: ${color}`));
