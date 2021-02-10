a413d4eac9384b73b946e13675d50565ebf9c0e1
reme3d2y <AYatsenko@alfabank.ru>
2021-02-10 10:30:42 +0300

    feat(icon-button): add component

---
+++ packages/icon-button/src/Component.test.tsx
@@ -0,0 +1,102 @@
+import React, { MouseEvent } from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import { StarMIcon } from '@alfalab/icons-glyph';
+
+import { IconButton } from './index';
+
+describe('IconButton', () => {
+    describe('Snapshots tests', () => {
+        it('should match snapshot', () => {
+            expect(render(<IconButton icon={StarMIcon} />)).toMatchSnapshot();
+        });
+    });
+
+    describe('Attributes tests', () => {
+        it('should set `data-test-id` attribute', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(<IconButton icon={StarMIcon} dataTestId={dataTestId} />);
+
+            expect(getByTestId(dataTestId).tagName).toBe('BUTTON');
+        });
+    });
+
+    describe('Classes tests', () => {
+        it('should set `className` class', () => {
+            const className = 'test-class';
+            const { container } = render(<IconButton icon={StarMIcon} className={className} />);
+
+            expect(container.firstElementChild).toHaveClass(className);
+        });
+
+        it('should set `size` class on icon wrapper', () => {
+            const size = 'm';
+            const { container } = render(<IconButton icon={StarMIcon} size={size} />);
+
+            expect(container.querySelector('.iconWrapper')).toHaveClass(size);
+        });
+
+        it('should set `view` class', () => {
+            const view = 'primary';
+            const { container } = render(<IconButton icon={StarMIcon} view={view} />);
+
+            expect(container.firstElementChild).toHaveClass(view);
+        });
+    });
+
+    describe('Callbacks tests', () => {
+        it('should call `onClick` prop', () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <IconButton icon={StarMIcon} onClick={cb} dataTestId={dataTestId} />,
+            );
+
+            fireEvent.click(getByTestId(dataTestId));
+
+            expect(cb).toBeCalledTimes(1);
+        });
+
+        it('should not call `onClick` prop if disabled', () => {
+            const cb = jest.fn();
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <IconButton
+                    icon={StarMIcon}
+                    onClick={cb}
+                    dataTestId={dataTestId}
+                    disabled={true}
+                />,
+            );
+
+            fireEvent.click(getByTestId(dataTestId));
+
+            expect(cb).not.toBeCalled();
+        });
+
+        /**
+         * Тест нужен для проверки типа eventTarget (HTMLIconButtonElement/HTMLAnchorElement).
+         * Если тест скомпилился - то все ок.
+         */
+        it('target should contain button element', () => {
+            const dataTestId = 'test-id';
+            const { getByTestId } = render(
+                <IconButton icon={StarMIcon} onClick={cb} dataTestId={dataTestId} />,
+            );
+
+            const buttonNode = getByTestId(dataTestId);
+
+            function cb(event: MouseEvent<HTMLButtonElement>) {
+                expect(event.target).toBe(buttonNode);
+            }
+
+            fireEvent.click(buttonNode, { target: buttonNode });
+        });
+    });
+
+    it('should unmount without errors', () => {
+        const { unmount } = render(<IconButton icon={StarMIcon}>Text</IconButton>);
+
+        expect(unmount).not.toThrowError();
+    });
+});
--- packages/icon-button/src/Component.tsx
+++ packages/icon-button/src/Component.tsx
@@ -1,26 +1,46 @@
 import React, { ButtonHTMLAttributes, ElementType, forwardRef } from 'react';
 import cn from 'classnames';
 
-import { Button, ComponentProps } from '@alfalab/core-components-button';
+import { Button } from '@alfalab/core-components-button';
 
 import styles from './index.module.css';
 
-export type IconButtonProps = ComponentProps &
-    ButtonHTMLAttributes<HTMLButtonElement> & {
-        icon: ElementType<{ className?: string }>;
+export type IconButtonProps = {
+    /**
+     * Компонент иконки
+     */
+    icon: ElementType<{ className?: string }>;
 
-        view?: 'primary' | 'secondary' | 'transparent';
+    /**
+     * Тип кнопки
+     */
+    view?: 'primary' | 'secondary' | 'transparent';
 
-        size?: 's' | 'm';
-    };
+    /**
+     * Размер компонента
+     */
+    size?: 's' | 'm';
+
+    /**
+     * Дополнительный класс
+     */
+    className?: string;
+
+    /**
+     * Идентификатор для систем автоматизированного тестирования
+     */
+    dataTestId?: string;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
-    ({ className, icon: Icon, view = 'primary', size = 's' }, ref) => {
+    ({ className, icon: Icon, view = 'primary', size = 's', ...restProps }, ref) => {
         return (
             <Button
+                {...restProps}
                 ref={ref}
                 view='ghost'
                 className={cn(className, styles[view])}
+                size={size}
                 leftAddons={
                     <span className={cn(styles.iconWrapper, styles[size])}>
                         <Icon className={styles.icon} />
