51d6d1ca8c08c82fe85861583f5d60cc513f04b7
Демичев Андрей Валерьевич <ADemichev@alfabank.ru>
2021-03-01 17:23:49 +0200

    fix(select-with-tags): added description

--- packages/select-with-tags/src/components/tag-list/component.tsx
+++ packages/select-with-tags/src/components/tag-list/component.tsx
@@ -174,6 +174,13 @@ export const TagList: FC<FieldProps & FormControlProps & TagListOwnProps> = ({
 
     const filled = Boolean(selectedMultiple.length > 0) || Boolean(value);
 
+    /**
+     * Флаг который позволит добавлять класс с вертикальными
+     * отступами если элементы не помещаются в один ряд,
+     * для того чтобы не менялась высота инпута
+     */
+    const shouldAddVerticalMargin = Boolean((!collapseTagList || isShowMoreEnabled) && !label);
+
     return (
         <div
             ref={wrapperRef}
@@ -203,9 +210,7 @@ export const TagList: FC<FieldProps & FormControlProps & TagListOwnProps> = ({
                     className={cn(styles.contentWrapper, {
                         [styles.hasLabel]: Boolean(label),
                         [styles.hasTags]: selectedMultiple.length > 0,
-                        [styles.contentWrapperVertical]: Boolean(
-                            (!collapseTagList || isShowMoreEnabled) && !label,
-                        ),
+                        [styles.contentWrapperVertical]: shouldAddVerticalMargin,
                     })}
                     ref={contentWrapperRef}
                 >
