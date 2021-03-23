ffbf3cec2f22a5752c7ff67a3f72ba1c8715e280
reme3d2y <AYatsenko@alfabank.ru>
2021-03-23 12:24:30 +0300

    feat(alert): max content width

--- packages/alert/src/Component.tsx
+++ packages/alert/src/Component.tsx
@@ -7,11 +7,13 @@ import styles from './index.module.css';
 
 export type AlertProps = Omit<PlateProps, 'foldable' | 'defaultFolded' | 'leftAddons'>;
 
-export const Alert: FC<AlertProps> = ({ className, title, ...restProps }) => (
+export const Alert: FC<AlertProps> = ({ className, title, children, ...restProps }) => (
     <Plate
         className={cn(styles.component, className)}
         buttonsClassName={styles.buttons}
         title={title ? <span className={styles.title}>{title}</span> : null}
         {...restProps}
-    />
+    >
+        <div className={styles.content}>{children}</div>
+    </Plate>
 );
