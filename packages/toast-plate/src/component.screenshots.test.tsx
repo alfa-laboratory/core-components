5c7ef85d5308165db775356dfd455a7f7947b89c
reme3d2y <AYatsenko@alfabank.ru>
2021-03-31 10:54:31 +0300

    chore(toast-plate): add themes

--- packages/toast-plate/src/component.screenshots.test.tsx
+++ packages/toast-plate/src/component.screenshots.test.tsx
@@ -2,6 +2,7 @@ import {
     setupScreenshotTesting,
     createSpriteStorybookUrl,
     generateTestCases,
+    customSnapshotIdentifier,
 } from '../../screenshot-utils';
 
 const screenshotTesting = setupScreenshotTesting({
@@ -13,44 +14,54 @@ const screenshotTesting = setupScreenshotTesting({
 
 const clip = { x: 0, y: 0, width: 480, height: 100 };
 
-describe(
-    'ToastPlate | main props',
-    screenshotTesting({
-        cases: [
-            [
-                'sprite',
-                createSpriteStorybookUrl({
-                    componentName: 'ToastPlate',
-                    knobs: {
-                        children: 'Вам одобрено. Согласитесь на предложение',
-                        badge: ['attention', 'positive', 'negative', ''],
-                        title: ['', 'Поздравляем, полный успех'],
-                        hasCloser: [false, true],
-                    },
-                    size: clip,
-                }),
+describe('ToastPlate | main props', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    `${theme} theme`,
+                    createSpriteStorybookUrl({
+                        componentName: 'ToastPlate',
+                        knobs: {
+                            children: 'Вам одобрено. Согласитесь на предложение',
+                            badge: ['attention', 'positive', 'negative', ''],
+                            title: ['', 'Поздравляем, полный успех'],
+                            hasCloser: [false, true],
+                        },
+                        size: clip,
+                    }),
+                ],
             ],
-        ],
-        screenshotOpts: {
-            fullPage: true,
-        },
-    }),
-);
-
-describe(
-    'ToastPlate | action button',
-    screenshotTesting({
-        cases: generateTestCases({
-            componentName: 'ToastPlate',
-            testStory: false,
-            knobs: {
-                children: 'Вам одобрено',
-                title: 'Поздравляем',
-                renderActionButton: true,
+            screenshotOpts: {
+                fullPage: true,
             },
-        }),
-        screenshotOpts: {
-            clip,
-        },
-    }),
-);
+            theme,
+        })();
+
+    ['default', 'click'].map(testCase);
+});
+
+describe('ToastPlate | action button', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: generateTestCases({
+                componentName: 'ToastPlate',
+                testStory: false,
+                knobs: {
+                    children: 'Вам одобрено',
+                    title: 'Поздравляем',
+                    renderActionButton: true,
+                },
+            }),
+            screenshotOpts: {
+                clip,
+            },
+            matchImageSnapshotOptions: {
+                customSnapshotIdentifier: (...args) =>
+                    `${theme}-${customSnapshotIdentifier(...args)}`,
+            },
+            theme,
+        })();
+
+    ['default', 'click'].map(testCase);
+});
