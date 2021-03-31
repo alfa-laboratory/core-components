6d0081d766f4c4242373d57b15c7091cba63b7e3
reme3d2y <AYatsenko@alfabank.ru>
2021-03-31 11:10:25 +0300

    chore(tag): add themes

--- packages/tag/src/component.screenshots.test.tsx
+++ packages/tag/src/component.screenshots.test.tsx
@@ -2,6 +2,7 @@ import {
     setupScreenshotTesting,
     createSpriteStorybookUrl,
     generateTestCases,
+    customSnapshotIdentifier,
 } from '../../screenshot-utils';
 
 const screenshotTesting = setupScreenshotTesting({
@@ -11,46 +12,56 @@ const screenshotTesting = setupScreenshotTesting({
     expect,
 });
 
-describe(
-    'Tag | sprite',
-    screenshotTesting({
-        cases: [
-            [
-                'main props',
-                createSpriteStorybookUrl({
-                    componentName: 'Tag',
-                    knobs: {
-                        children: 'Оплатить',
-                        size: ['xl', 'l', 'm', 's', 'xs'],
-                        checked: [false, true],
-                        disabled: [false, true],
-                    },
-                    size: { width: 160, height: 90 },
-                }),
+describe('Tag | main props', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: [
+                [
+                    theme,
+                    createSpriteStorybookUrl({
+                        componentName: 'Tag',
+                        knobs: {
+                            children: 'Оплатить',
+                            size: ['xl', 'l', 'm', 's', 'xs'],
+                            checked: [false, true],
+                            disabled: [false, true],
+                        },
+                        size: { width: 160, height: 90 },
+                    }),
+                ],
             ],
-        ],
-        screenshotOpts: {
-            fullPage: true,
-        },
-        viewport: { width: 740, height: 100 },
-    }),
-);
-
-describe(
-    'Tag | right addons',
-    screenshotTesting({
-        cases: generateTestCases({
-            componentName: 'Tag',
-            testStory: false,
-            knobs: {
-                rightAddons: true,
-                checked: [false, true],
-                disabled: [false, true],
+            screenshotOpts: {
+                fullPage: true,
             },
-        }),
-        screenshotOpts: {
-            fullPage: true,
-        },
-        viewport: { width: 180, height: 80 },
-    }),
-);
+            viewport: { width: 740, height: 100 },
+            theme,
+        })();
+
+    ['default', 'click'].map(testCase);
+});
+
+describe('Tag | right addons', () => {
+    const testCase = (theme: string) =>
+        screenshotTesting({
+            cases: generateTestCases({
+                componentName: 'Tag',
+                testStory: false,
+                knobs: {
+                    rightAddons: true,
+                    checked: [false, true],
+                    disabled: [false, true],
+                },
+            }),
+            screenshotOpts: {
+                fullPage: true,
+            },
+            matchImageSnapshotOptions: {
+                customSnapshotIdentifier: (...args) =>
+                    `${theme}-${customSnapshotIdentifier(...args)}`,
+            },
+            viewport: { width: 180, height: 80 },
+            theme,
+        })();
+
+    ['default', 'click'].map(testCase);
+});
