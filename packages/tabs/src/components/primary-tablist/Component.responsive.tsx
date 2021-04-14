bb67e5da7eb1208b96e0461fc6711b27689cf891
Evgeny Bykovskikh <redzumi.tao@gmail.com>
2021-04-15 02:04:30 +0300

    feat(tabs): make todo done

--- packages/tabs/src/components/primary-tablist/Component.responsive.tsx
+++ packages/tabs/src/components/primary-tablist/Component.responsive.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useMedia } from '@alfalab/hooks';
 import { PrimaryTabListDesktop } from './Component.desktop';
 import { PrimaryTabListMobile } from './Component.mobile';
 import { TabListProps } from '../../typings';
-import { useWindowWidth } from '../../utils';
+
+type View = 'desktop' | 'mobile';
 
 export const PrimaryTabListResponsive = ({ size, ...restProps }: TabListProps) => {
-    return useWindowWidth() >= 768 ? (
+    const [view] = useMedia<View>(
+        [
+            ['mobile', '(max-width: 767px)'],
+            ['desktop', '(min-width: 768px)'],
+        ],
+        'desktop',
+    );
+
+    return view === 'desktop' ? (
         <PrimaryTabListDesktop size={size} {...restProps} />
     ) : (
         <PrimaryTabListMobile {...restProps} />
--- packages/tabs/src/components/secondary-tablist/Component.responsive.tsx
+++ packages/tabs/src/components/secondary-tablist/Component.responsive.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useMedia } from '@alfalab/hooks';
 import { SecondaryTabListDesktop } from './Component.desktop';
 import { SecondaryTabListMobile } from './Component.mobile';
 import { SecondaryTabListProps } from '../../typings';
-import { useWindowWidth } from '../../utils';
+
+type View = 'desktop' | 'mobile';
 
 export const SecondaryTabListResponsive = ({ size, ...restProps }: SecondaryTabListProps) => {
-    return useWindowWidth() >= 768 ? (
+    const [view] = useMedia<View>(
+        [
+            ['mobile', '(max-width: 767px)'],
+            ['desktop', '(min-width: 768px)'],
+        ],
+        'desktop',
+    );
+
+    return view === 'desktop' ? (
         <SecondaryTabListDesktop size={size} {...restProps} />
     ) : (
         <SecondaryTabListMobile {...restProps} />
