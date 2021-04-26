77db054b18c1dba6713d9a9d99c95a1ef5c98e8e
Alexander Yatsenko <reme3d2y@gmail.com>
2021-04-26 13:26:48 +0300

    feat(modal): fixed position (#617)

--- packages/modal/src/Component.desktop.tsx
+++ packages/modal/src/Component.desktop.tsx
@@ -1,7 +1,9 @@
-import React, { cloneElement, forwardRef, isValidElement } from 'react';
+import React, { cloneElement, forwardRef, isValidElement, useCallback, useRef } from 'react';
 import cn from 'classnames';
+import { TransitionProps } from 'react-transition-group/Transition';
 
 import { BaseModal, BaseModalProps } from '@alfalab/core-components-base-modal';
+import mergeRefs from 'react-merge-refs';
 import { HeaderDesktop } from './components/header/Component.desktop';
 import { ContentDesktop } from './components/content/Component.desktop';
 import { FooterDesktop } from './components/footer/Component.desktop';
@@ -23,6 +25,12 @@ export type ModalDesktopProps = BaseModalProps & {
     fullscreen?: boolean;
 
     /**
+     * Фиксирует позицию модального окна после открытия,
+     * предотвращая скачки, если контент внутри будет меняться
+     */
+    fixedPosition?: boolean;
+
+    /**
      * Управление наличием закрывающего крестика
      * @default false
      */
@@ -30,29 +38,66 @@ export type ModalDesktopProps = BaseModalProps & {
 };
 
 const ModalDesktopComponent = forwardRef<HTMLDivElement, ModalDesktopProps>(
-    ({ size = 's', fullscreen, children, className, wrapperClassName, ...restProps }, ref) => (
-        <BaseModal
-            {...restProps}
-            ref={ref}
-            wrapperClassName={cn(styles.wrapper, wrapperClassName, {
-                [styles.fullscreen]: fullscreen,
-            })}
-            className={cn(styles.component, className, !fullscreen && styles[size])}
-            backdropProps={{
-                invisible: fullscreen,
-            }}
-            transitionProps={{
-                classNames: transitions,
-                ...restProps.transitionProps,
-            }}
-        >
-            {React.Children.map(children, child =>
-                isValidElement(child)
-                    ? cloneElement(child, { size: child.props.size || size, fullscreen })
-                    : child,
-            )}
-        </BaseModal>
-    ),
+    (
+        {
+            size = 's',
+            fixedPosition,
+            fullscreen,
+            children,
+            className,
+            wrapperClassName,
+            transitionProps = {},
+            ...restProps
+        },
+        ref,
+    ) => {
+        const modalRef = useRef<HTMLElement>(null);
+
+        const handleEntered: TransitionProps['onEntered'] = useCallback(
+            (node, isAppearing) => {
+                if (fixedPosition && modalRef.current) {
+                    const content = modalRef.current.querySelector<HTMLElement>(
+                        `.${styles.component}`,
+                    );
+
+                    if (content) {
+                        const { marginTop } = window.getComputedStyle(content);
+                        content.style.marginTop = marginTop;
+                    }
+                }
+
+                if (transitionProps.onEntered) {
+                    transitionProps.onEntered(node, isAppearing);
+                }
+            },
+            [fixedPosition, transitionProps],
+        );
+
+        return (
+            <BaseModal
+                {...restProps}
+                ref={mergeRefs([ref, modalRef])}
+                wrapperClassName={cn(styles.wrapper, wrapperClassName, {
+                    [styles.fullscreen]: fullscreen,
+                })}
+                className={cn(styles.component, className, !fullscreen && styles[size])}
+                backdropProps={{
+                    invisible: fullscreen,
+                }}
+                transitionProps={{
+                    classNames: transitions,
+                    ...transitionProps,
+                    onEntered: handleEntered,
+                }}
+            >
+                {React.Children.map(children, child =>
+                    isValidElement(child)
+                        ? cloneElement(child, { size: child.props.size || size, fullscreen })
+                        : child,
+                )}
+            </BaseModal>
+        );
+    },
 );
 
 export const ModalDesktop = Object.assign(ModalDesktopComponent, {
