85decac3eb3111066bada7193dafa2d0e433a093
reme3d2y <AYatsenko@alfabank.ru>
2021-05-05 17:10:16 +0300

    feat(drawer): update animtaion

--- packages/drawer/src/Component.tsx
+++ packages/drawer/src/Component.tsx
@@ -1,4 +1,6 @@
 import React, { forwardRef, useMemo } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { TransitionProps } from 'react-transition-group/Transition';
 import cn from 'classnames';
 
 import { BaseModal, BaseModalProps, BaseModalContext } from '@alfalab/core-components-base-modal';
@@ -7,15 +9,27 @@ import styles from './index.module.css';
 
 export const ANIMATION_DURATION = 600;
 
-export type DrawerProps = Omit<BaseModalProps, 'container'>;
+export type DrawerProps = Omit<BaseModalProps, 'container'> & {
+    /**
+     * Пропсы для анимации контента (CSSTransition)
+     */
+    contentTransitionProps?: Partial<TransitionProps>;
+};
 
 export const DrawerContext = BaseModalContext;
 
 export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
-    ({ open, className, children, ...restProps }, ref) => {
+    ({ open, className, children, contentTransitionProps, ...restProps }, ref) => {
         const transitionProps = useMemo(
             () => ({
-                classNames: styles,
+                classNames: {
+                    enter: styles.enter,
+                    appear: styles.enter,
+                    enterActive: styles.enterActive,
+                    appearActive: styles.enterActive,
+                    exit: styles.exit,
+                    exitActive: styles.exitActive,
+                },
                 timeout: ANIMATION_DURATION,
                 ...restProps.transitionProps,
             }),
@@ -26,11 +40,11 @@ export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
             () => ({
                 classNames: {
                     enter: styles.backdropEnter,
+                    appear: styles.backdropEnter,
                     enterActive: styles.backdropEnterActive,
+                    appearActive: styles.backdropEnterActive,
                     enterDone: styles.backdropEnterDone,
-                    appear: styles.backdropAppear,
-                    appearActive: styles.backdropAppearActive,
-                    appearDone: styles.backdropAppearDone,
+                    appearDone: styles.backdropEnterDone,
                     exit: styles.backdropExit,
                     exitActive: styles.backdropExitActive,
                     exitDone: styles.backdropExitDone,
@@ -39,6 +53,22 @@ export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
                 ...restProps.backdropProps,
             }),
             [restProps.backdropProps],
+        );
+
+        const contentProps = useMemo(
+            () => ({
+                classNames: {
+                    enter: styles.contentEnter,
+                    appear: styles.contentEnter,
+                    enterActive: styles.contentEnterActive,
+                    appearActive: styles.contentEnterActive,
+                    exit: styles.contentExit,
+                    exitActive: styles.contentExitActive,
+                },
+                timeout: ANIMATION_DURATION,
+                ...contentTransitionProps,
+            }),
+            [contentTransitionProps],
         );
 
         return (
@@ -51,7 +81,9 @@ export const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
                 transitionProps={transitionProps}
                 backdropProps={backdropProps}
             >
-                {children}
+                <CSSTransition appear={true} {...contentProps} in={open}>
+                    <div className={styles.content}>{children}</div>
+                </CSSTransition>
             </BaseModal>
         );
     },
