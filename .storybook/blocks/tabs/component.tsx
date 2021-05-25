0663beab57a701c53ac41ee7df55240d7e6125ac
Dmitry Savkin <dmitrsavk@yandex.ru>
2021-05-25 12:42:34 +0300

    docs(button): add css-vars to storybook (#664)
    * docs(button): add css-vars to storybook
    
    * docs(button): save css formatting
    
    * docs(vars): add story
    
    * docs: add css-vars highlight
    
    * docs: add tabs for description, props and css-vars

---
+++ .storybook/blocks/tabs/component.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState, useMemo, useCallback, ReactNode } from 'react';
+import { TabsResponsive, Tab, TabsResponsiveProps } from '@alfalab/core-components-tabs';
+
+enum TabName {
+    DESCRIPTION = 'DESCRIPTION',
+    PROPS = 'PROPS',
+    CSS_VARS = 'CSS_VARS',
+}
+
+const TabTitle = {
+    [TabName.DESCRIPTION]: 'Описание',
+    [TabName.PROPS]: 'Свойства',
+    [TabName.CSS_VARS]: 'CSS-переменные',
+};
+
+type Props = {
+    description: ReactNode;
+    prpos: ReactNode;
+    cssVars?: ReactNode;
+    defaultSelected?: TabName;
+};
+
+export const Tabs: FC<Props> = ({
+    description,
+    prpos,
+    cssVars,
+    defaultSelected = TabName.DESCRIPTION,
+}) => {
+    const [selected, setSelected] = useState(defaultSelected);
+
+    const handleChange = useCallback<Required<TabsResponsiveProps>['onChange']>(
+        (_, { selectedId }) => {
+            setSelected(selectedId as TabName);
+        },
+        [],
+    );
+
+    const renderTabs = (): TabsResponsiveProps['children'] => {
+        const result = [
+            <Tab title={TabTitle[TabName.DESCRIPTION]} id={TabName.DESCRIPTION}>
+                <div style={{ marginTop: '40px' }}>{description}</div>
+            </Tab>,
+            <Tab title={TabTitle[TabName.PROPS]} id={TabName.PROPS}>
+                {prpos}
+            </Tab>,
+        ];
+
+        if (cssVars) {
+            result.push(
+                <Tab title={TabTitle[TabName.CSS_VARS]} id={TabName.CSS_VARS}>
+                    <div style={{ marginTop: '40px' }}>{cssVars}</div>
+                </Tab>,
+            );
+        }
+
+        return result;
+    };
+
+    const tabs = useMemo(() => renderTabs(), [description, prpos, cssVars]);
+
+    return (
+        <TabsResponsive selectedId={selected} onChange={handleChange}>
+            {tabs}
+        </TabsResponsive>
+    );
+};
