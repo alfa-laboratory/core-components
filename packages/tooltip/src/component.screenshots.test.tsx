9f03cd5956dd8b17025ee9947a10bc059a339846
Artem Kononenko <Artess999@users.noreply.github.com>
2021-06-07 17:13:41 +0300

    feat(tooltip): add hint view (#688)
    * feat(tooltip): add hint view
    
    * chore(tooltip): hint review fix
    
    * test(tooltip): add hint view screenshot
    
    Co-authored-by: Кононенко Артем Игоревич <AIKononenko@alfabank.ru>

--- packages/tooltip/src/component.screenshots.test.tsx
+++ packages/tooltip/src/component.screenshots.test.tsx
@@ -60,4 +60,45 @@ describe('Tooltip', () => {
 
         await closeBrowser({ browser, context, page });
     });
+
+    test('test hint view', async () => {
+        jest.setTimeout(60000);
+
+        const cases = generateTestCases({
+            componentName: 'Tooltip',
+            testStory: false,
+            knobs: {
+                open: true,
+                onOpenDelay: 0,
+                position: 'top',
+                view: 'hint',
+            },
+        });
+
+        const { browser, context, page, css } = await openBrowserPage(cases[0][1]);
+
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        for (const [_, pageUrl] of cases) {
+            try {
+                await page.goto(pageUrl);
+
+                await page.hover('*[class^=target]');
+
+                await matchHtml({
+                    page,
+                    expect,
+                    css,
+                    matchImageSnapshotOptions: {
+                        failureThresholdType: 'percent',
+                        failureThreshold: 1.5,
+                    },
+                });
+            } catch (error) {
+                // eslint-disable-next-line no-console
+                await console.error(error.message);
+            }
+        }
+
+        await closeBrowser({ browser, context, page });
+    });
 });
