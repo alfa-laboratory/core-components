7cb826a458d44454fa1ddec5966eb732e7f48a3f
Mikhail Koretsky <koretsky97@gmail.com>
2021-06-18 16:56:55 +0300

    feat(calendar-range): fully adds popover variant

---
+++ packages/calendar-range/src/useCalendarMaxMinDates.ts
@@ -0,0 +1,36 @@
+import { startOfMonth, endOfMonth } from 'date-fns';
+
+import { limitDate } from '@alfalab/core-components-calendar';
+
+type useMaxMinDatesProps = {
+    isPopover: boolean;
+    monthTo: number;
+    monthFrom: number;
+    selectedTo?: number;
+    selectedFrom?: number;
+    maxDate?: number;
+    minDate?: number;
+};
+
+export function useCalendarMaxMinDates({
+    isPopover,
+    monthTo,
+    monthFrom,
+    selectedTo,
+    selectedFrom,
+    maxDate,
+    minDate,
+}: useMaxMinDatesProps) {
+    const popoverFromMaxDate = selectedTo;
+    const fromMaxDate = limitDate(endOfMonth(monthFrom), minDate, maxDate).getTime();
+
+    const popoverToMinDate = selectedFrom;
+    const toMinDate = limitDate(startOfMonth(monthTo), minDate, maxDate).getTime();
+
+    return {
+        fromMinDate: minDate,
+        toMaxDate: maxDate,
+        fromMaxDate: isPopover ? popoverFromMaxDate : fromMaxDate,
+        toMinDate: isPopover ? popoverToMinDate : toMinDate,
+    };
+}
