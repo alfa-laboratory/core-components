207d3d30171a84ff4021cd119c0da1316a92d14c
Askhat Arslanov <AArslanov2@alfabank.ru>
2021-07-07 12:27:38 +0300

    fix(badge): fix name

--- packages/badge/src/Component.tsx
+++ packages/badge/src/Component.tsx
@@ -35,7 +35,7 @@ export type BadgeProps = {
     iconColor?:
         | 'positive'
         | 'attention'
-        | 'neutral'
+        | 'link'
         | 'negative'
         | 'tertiary'
         | 'secondary'
