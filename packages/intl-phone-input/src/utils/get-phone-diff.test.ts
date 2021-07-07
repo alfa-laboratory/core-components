b2211c36d215eb1792f75f2012d90536bfc2f59d
Mikhail Koretsky <koretsky97@gmail.com>
2021-07-07 13:55:30 +0300

    fix(intl-phone-input): add logic to getPhoneDiff

--- packages/intl-phone-input/src/utils/get-phone-diff.test.ts
+++ packages/intl-phone-input/src/utils/get-phone-diff.test.ts
@@ -1,30 +1,38 @@
 import { Country } from '@alfalab/utils';
 import { getPhoneDiff } from './get-phone-diff';
 
-describe('getPhoneDiff', () => {
-    it('should work', () => {
-        const ru = {
-            dialCode: '7',
-        } as Country;
+const ru = {
+    dialCode: '7',
+} as Country;
 
-        const az = {
-            dialCode: '994',
-        } as Country;
-        expect(getPhoneDiff('', ru)).toEqual(null);
-        expect(getPhoneDiff('+', ru)).toEqual(null);
-        expect(getPhoneDiff('7', ru)).toEqual(null);
-        expect(getPhoneDiff('+7', ru)).toEqual(null);
+const az = {
+    dialCode: '994',
+} as Country;
+
+describe('getPhoneDiff', () => {
+    it('should work with short country code', () => {
+        expect(getPhoneDiff('', ru)).toEqual('');
+        expect(getPhoneDiff('+', ru)).toEqual('');
+        expect(getPhoneDiff('7', ru)).toEqual('');
+        expect(getPhoneDiff('+7', ru)).toEqual('');
+        expect(getPhoneDiff('+71', ru)).toEqual('');
         expect(getPhoneDiff('+17', ru)).toEqual('1');
         expect(getPhoneDiff('1+7', ru)).toEqual('1');
+        expect(getPhoneDiff('+1', ru)).toEqual('1');
+    });
 
-        expect(getPhoneDiff('', az)).toEqual(null);
-        expect(getPhoneDiff('+', az)).toEqual(null);
-        expect(getPhoneDiff('+9', az)).toEqual(null);
-        expect(getPhoneDiff('+99', az)).toEqual(null);
-        expect(getPhoneDiff('+994', az)).toEqual(null);
+    it('should work with long country code', () => {
+        expect(getPhoneDiff('', az)).toEqual('');
+        expect(getPhoneDiff('+', az)).toEqual('');
+        expect(getPhoneDiff('+9', az)).toEqual('');
+        expect(getPhoneDiff('+99', az)).toEqual('');
+        expect(getPhoneDiff('+994', az)).toEqual('');
         expect(getPhoneDiff('1+994', az)).toEqual('1');
         expect(getPhoneDiff('+1994', az)).toEqual('1');
         expect(getPhoneDiff('+9194', az)).toEqual('1');
         expect(getPhoneDiff('+9914', az)).toEqual('1');
+        expect(getPhoneDiff('+993', az)).toEqual('3');
+        expect(getPhoneDiff('+934', az)).toEqual('3');
+        expect(getPhoneDiff('+394', az)).toEqual('3');
     });
 });
--- packages/intl-phone-input/src/utils/get-phone-diff.ts
+++ packages/intl-phone-input/src/utils/get-phone-diff.ts
@@ -4,8 +4,26 @@ import { Country } from '@alfalab/utils';
  * Возвращает разницу между значением и кодом страны
  */
 export const getPhoneDiff = (value: string, country: Country) => {
-    if (!value || value === `+${country.dialCode}`) return null;
+    if (!value || value === `+${country.dialCode}`) return '';
 
+    const insertionDiff = getInsertionDiff(value, country);
+
+    if (insertionDiff) {
+        return insertionDiff;
+    }
+
+    const noPlusValue = value.startsWith('+') ? value.slice(1) : value;
+    const changeDiff = getChangeDiff(country.dialCode, noPlusValue);
+
+    if (changeDiff) {
+        return changeDiff;
+    }
+
+    return '';
+};
+
+// Находит символы вставляемые в код страны
+function getInsertionDiff(value: string, country: Country) {
     const dialRegex = country.dialCode.split('').reduce((acc, item, index) => {
         return `${acc}(?<dial${index}>\\d+)*${item}`;
     }, '');
@@ -20,4 +38,19 @@ export const getPhoneDiff = (value: string, country: Country) => {
     if (!groups) return null;
 
     return Object.values(groups).join('');
-};
+}
+
+// Находит символы вводимые выделением и заменой части кода страны
+function getChangeDiff(template: string, value: string) {
+    let result = '';
+
+    for (let i = 0; i < template.length; i++) {
+        const targetChar = value[i];
+
+        if (targetChar && template[i] !== targetChar) {
+            result += value[i];
+        }
+    }
+
+    return result;
+}
