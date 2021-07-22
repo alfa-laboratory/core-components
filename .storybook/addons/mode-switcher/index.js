02e67a74af32041b9b3d39a4d0d6996b9b50d886
reme3d2y <AYatsenko@alfabank.ru>
2021-07-22 17:17:42 +0300

    chore: add mode switcher

---
+++ .storybook/addons/mode-switcher/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import addons, { makeDecorator } from '@storybook/addons';
+import { ADDON_ID } from './register';
+import { DarkThemeStylesInjector } from '../../../packages/dark-theme-styles-injector';
+
+export default makeDecorator({
+    name: 'withModeSwitcher',
+    wrapper: (getStory, context) => {
+        const [mode, setMode] = useState('light');
+        const channel = addons.getChannel();
+
+        useEffect(() => {
+            channel.on(`${ADDON_ID}/mode`, setMode);
+
+            return () => {
+                channel.removeListener(`${ADDON_ID}/mode`, setMode);
+            };
+        }, [channel]);
+
+        console.log(mode);
+
+        return (
+            <>
+                {mode === 'dark' && <DarkThemeStylesInjector colors='indigo' />}
+                {getStory(context)}
+            </>
+        );
+    },
+});
