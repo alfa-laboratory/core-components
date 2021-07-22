deddd82b45ca924e89a93cb4bd37e5d4c7f4b699
reme3d2y <AYatsenko@alfabank.ru>
2021-07-22 17:48:37 +0300

    chore: fix storybook

--- .storybook/addons/mode-switcher/index.js
+++ .storybook/addons/mode-switcher/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import addons, { makeDecorator } from '@storybook/addons';
 import { ADDON_ID } from './register';
-import { DarkThemeStylesInjector } from '../../../packages/dark-theme-styles-injector';
+import { DarkThemeStylesInjector } from '../../../packages/dark-theme-styles-injector/src/index';
 
 export default makeDecorator({
     name: 'withModeSwitcher',
