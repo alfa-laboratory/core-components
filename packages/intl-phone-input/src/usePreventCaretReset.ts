ea4b60bcc45bbaed46ac5b7ee7f6f4a05cf9f004
Mikhail <koretsky97@gmail.com>
2021-08-09 15:10:14 +0300

    Доработки IntlPhoneInput (#771)
    * fix(intl-phone-input): type when country code selected
    * fix(intl-phone-input): caret reset on delete chars inside string

---
+++ packages/intl-phone-input/src/usePreventCaretReset.ts
@@ -0,0 +1,45 @@
+import { RefObject, useCallback, useEffect } from 'react';
+
+type Args = {
+    inputRef: RefObject<HTMLInputElement>;
+    countryCodeLength: number;
+    clearableCountryCode: boolean;
+};
+
+export function usePreventCaretReset({ inputRef, clearableCountryCode, countryCodeLength }: Args) {
+    const handleDeleteChar = useCallback(
+        (event: KeyboardEvent) => {
+            const input = event.target as HTMLInputElement;
+            const caretPosition = input.selectionStart;
+
+            if (event.key !== 'Backspace' || !caretPosition) return;
+
+            if (!clearableCountryCode && caretPosition <= countryCodeLength) {
+                event.preventDefault();
+
+                return;
+            }
+
+            const newPosition = caretPosition - 1;
+
+            requestAnimationFrame(() => {
+                input.setSelectionRange(newPosition, newPosition);
+            });
+        },
+        [clearableCountryCode, countryCodeLength],
+    );
+
+    useEffect(() => {
+        const input = inputRef.current;
+
+        if (!input) return;
+
+        input.addEventListener('keydown', handleDeleteChar);
+
+        // eslint-disable-next-line consistent-return
+        return () => {
+            input.removeEventListener('keydown', handleDeleteChar);
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [handleDeleteChar]);
+}
