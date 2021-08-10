aa23e6f74a657a511a0c4ca1713ff4f4467cc231
reme3d2y <AYatsenko@alfabank.ru>
2021-08-10 13:33:09 +0300

    feat: remove var duplicates in theme mixins

--- packages/themes/src/compiled/mobile-dark-bluetint.ts
+++ packages/themes/src/compiled/mobile-dark-bluetint.ts
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/no-webpack-loader-syntax
-import styles from '!!css-loader!@alfalab/core-components/themes/dark/compiled-mobile-dark-bluetint.css';
+import styles from '!!css-loader!@alfalab/core-components/themes/compiled/mobile-dark-bluetint.css';
 
 export default styles;
