366ffd3fd8e829a3c8d85bc9a90c55a1ff0607ab
Mikhail <koretsky97@gmail.com>
2021-08-17 10:12:33 +0300

    Feat/gallery header (#774)
    * feat(gallery): add gallery header
    
    * test(gallery): add test and fix for split-filename util

---
+++ packages/gallery/src/components/header-info-block/Component.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Typography } from '@alfalab/core-components-typography';
+
+import styles from './index.module.css';
+import { splitFilename } from '../../utils';
+
+export type Props = {
+    filename: string;
+    description?: string;
+};
+
+export const HeaderInfoBlock: React.FunctionComponent<Props> = ({ filename, description }) => {
+    const [head, tail] = splitFilename(filename);
+
+    return (
+        <div className={styles.info}>
+            <span className={styles.filenameContainer}>
+                <Typography.Title
+                    tag='h1'
+                    className={styles.filenameHead}
+                    view='xsmall'
+                    font='system'
+                    color='primary-inverted'
+                >
+                    {head}
+                </Typography.Title>
+                <Typography.Title tag='h1' view='xsmall' font='system' color='primary-inverted'>
+                    {tail}
+                </Typography.Title>
+            </span>
+            {description ? (
+                <Typography.Text
+                    className={styles.description}
+                    tag='div'
+                    view='primary-medium'
+                    color='secondary-inverted'
+                >
+                    {description}
+                </Typography.Text>
+            ) : null}
+        </div>
+    );
+};
