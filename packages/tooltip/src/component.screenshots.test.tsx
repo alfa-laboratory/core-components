d5fe8724c090273553bf1a3a12c5a7f4efb6278f
Evgeny Sergeev <siebensieben@gmail.com>
2021-09-01 19:44:15 +0300

    updated screenshots

--- packages/tooltip/src/component.screenshots.test.tsx
+++ packages/tooltip/src/component.screenshots.test.tsx
@@ -49,7 +49,7 @@ describe('Tooltip', () => {
                     css,
                     matchImageSnapshotOptions: {
                         failureThresholdType: 'percent',
-                        failureThreshold: 1.5,
+                        failureThreshold: 0,
                     },
                 });
             } catch (error) {
