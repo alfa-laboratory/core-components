112cd95242e27f5065eb2a8349d3afbddfdee638
Evgeny Sergeev <siebensieben@gmail.com>
2021-10-08 11:33:49 +0300

    feat(select): переход к темизации цвета иконки
    удалена переменная --select-arrow-background, добавлена --select-arrow-color
    
    BREAKING CHANGE: удалена переменная --select-arrow-background

--- packages/select/src/components/arrow/Component.tsx
+++ packages/select/src/components/arrow/Component.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import cn from 'classnames';
+import { ChevronDownMIcon } from '@alfalab/icons-glyph/ChevronDownMIcon';
 import { ArrowProps } from '../../typings';
 
 import styles from './index.module.css';
 
 export const Arrow = ({ open, className }: ArrowProps) => (
-    <span className={cn(styles.arrow, className, { [styles.open]: open })} />
+    <ChevronDownMIcon className={cn(styles.arrow, className, { [styles.open]: open })} />
 );
