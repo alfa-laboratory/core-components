31afc82043e9cd2702dafa6714bab53c0e9a278a
Dmitry Savkin <dmitrsavk@yandex.ru>
2021-11-22 11:19:15 +0300

    docs(button): fix
    Co-authored-by: Alexander Yatsenko <reme3d2y@gmail.com>

--- packages/button/src/Component.tsx
+++ packages/button/src/Component.tsx
@@ -113,7 +113,7 @@ const logWarning = (view: Required<ComponentProps>['view']) => {
     console.warn(
         // eslint-disable-next-line prefer-template
         `@alfalab/core-components/button: view='${view}' будет удален в следующих мажорных версиях. ` +
-            `Используйте view='${viewsMap[view]}'. Чтобы поменять все кнопки на проекте разом, можно воспользоваться codemode: ` +
+            `Используйте view='${viewsMap[view]}'. Чтобы поменять все кнопки на проекте разом, можно воспользоваться codemod: ` +
             'npx core-components-codemod --components=ButtonViews src/**/*.tsx',
     );
 };
