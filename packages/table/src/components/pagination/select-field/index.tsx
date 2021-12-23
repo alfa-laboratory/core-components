e251f5a05b3e4e772e64c29d72bf034fce0671a8
reme3d2y <AYatsenko@alfabank.ru>
2021-12-23 09:49:55 +0300

    fix(table): popover lag

--- packages/table/src/components/pagination/select-field/index.tsx
+++ packages/table/src/components/pagination/select-field/index.tsx
@@ -6,14 +6,20 @@ import { Button, ButtonProps } from '@alfalab/core-components-button';
 
 import styles from './index.module.css';
 
-export const CustomSelectField: SelectProps['Field'] = ({ selected, innerProps, Arrow, open }) => (
-    <Button
-        {...(innerProps as ButtonProps)}
-        size='xxs'
-        view='link'
-        className={cn(styles.field, { [styles.open]: open })}
-        rightAddons={Arrow}
-    >
-        {selected?.content}
-    </Button>
-);
+export const CustomSelectField: SelectProps['Field'] = ({ selected, innerProps, Arrow, open }) => {
+    const { ref, ...restInnerProps } = innerProps;
+
+    return (
+        <div ref={ref}>
+            <Button
+                {...(restInnerProps as ButtonProps)}
+                size='xxs'
+                view='link'
+                className={cn(styles.field, { [styles.open]: open })}
+                rightAddons={Arrow}
+            >
+                {selected?.content}
+            </Button>
+        </div>
+    );
+};
