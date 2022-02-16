f824aab498402fceb0460d7c355a8eac69c7cca9
reme3d2y <AYatsenko@alfabank.ru>
2022-02-16 08:21:20 +0300

    chore(calendar): revert usePeriod

--- packages/calendar/src/usePeriod.ts
+++ packages/calendar/src/usePeriod.ts
@@ -1,6 +1,5 @@
 import { useCallback, useState } from 'react';
 import { useDidUpdateEffect } from '@alfalab/hooks';
-import { differenceInDays } from 'date-fns';
 
 type usePeriodProps = {
     /**
@@ -23,101 +22,7 @@ export function usePeriod({
     onPeriodChange,
     initialSelectedFrom,
     initialSelectedTo,
-}: usePeriodProps = {}) {
-    const [selectedFrom, setSelectedFrom] = useState<number | undefined>(initialSelectedFrom);
-    const [selectedTo, setSelectedTo] = useState<number | undefined>(initialSelectedTo);
-
-    const resetPeriod = useCallback(() => {
-        setSelectedFrom(undefined);
-        setSelectedTo(undefined);
-    }, []);
-
-    const setStart = useCallback((date?: number) => {
-        setSelectedFrom(date);
-    }, []);
-
-    const setEnd = useCallback((date?: number) => {
-        setSelectedTo(date);
-    }, []);
-
-    const updatePeriod = useCallback(
-        (date?: number) => {
-            // сбрасываем выделение
-            if (date === undefined || (date === selectedTo && date === selectedFrom)) {
-                resetPeriod();
-                return;
-            }
-
-            // сбрасываем конец, если выбранная дата совпадает с ним
-            if (date === selectedTo) {
-                setSelectedTo(undefined);
-                return;
-            }
-
-            // сбрасываем начало, если выбранная дата совпадает с ним
-            if (date === selectedFrom) {
-                if (selectedTo) {
-                    setSelectedFrom(selectedTo);
-                    setSelectedTo(undefined);
-                } else {
-                    setSelectedTo(date);
-                }
-                return;
-            }
-
-            if (!selectedFrom) {
-                if (selectedTo) {
-                    setSelectedFrom(Math.min(date, selectedTo));
-                    setSelectedTo(Math.max(date, selectedTo));
-                } else {
-                    // начинаем выделение
-                    setSelectedFrom(date);
-                }
-                return;
-            }
-
-            // заканчиваем выделение
-            if (!selectedTo) {
-                setSelectedFrom(Math.min(date, selectedFrom));
-                setSelectedTo(Math.max(date, selectedFrom));
-                return;
-            }
-
-            // сдвигаем тот конец выделения, который ближе к выбранной дате
-            if (
-                Math.abs(differenceInDays(date, selectedTo)) >
-                Math.abs(differenceInDays(date, selectedFrom))
-            ) {
-                setSelectedFrom(date);
-            } else {
-                setSelectedTo(date);
-            }
-        },
-        [resetPeriod, selectedFrom, selectedTo],
-    );
-
-    useDidUpdateEffect(() => {
-        if (onPeriodChange) {
-            onPeriodChange(selectedFrom, selectedTo);
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [selectedFrom, selectedTo]);
-
-    return {
-        selectedFrom,
-        selectedTo,
-        setStart,
-        setEnd,
-        resetPeriod,
-        updatePeriod,
-    };
-}
-
-export function usePeriodWithReset({
-    onPeriodChange,
-    initialSelectedFrom,
-    initialSelectedTo,
-}: usePeriodProps = {}) {
+}: usePeriodProps) {
     const [selectedFrom, setSelectedFrom] = useState<number | undefined>(initialSelectedFrom);
     const [selectedTo, setSelectedTo] = useState<number | undefined>(initialSelectedTo);
 
@@ -142,14 +47,8 @@ export function usePeriodWithReset({
                 return;
             }
 
-            if (!selectedFrom && selectedTo) {
-                setSelectedFrom(Math.min(date, selectedTo));
-                setSelectedTo(Math.max(date, selectedTo));
-                return;
-            }
-
+            // начинаем выделение
             if (!selectedFrom) {
-                // начинаем выделение
                 setSelectedFrom(date);
                 return;
             }
@@ -161,9 +60,20 @@ export function usePeriodWithReset({
                 return;
             }
 
-            // Если обе даты уже выбраны, начинаем выделение заново
-            setSelectedTo(undefined);
-            setSelectedFrom(date);
+            // если новая дата за выделенным периодом — расширяем интервал
+            if (date > selectedFrom) {
+                setSelectedTo(date);
+                return;
+            }
+
+            /**
+             * если новая дата находится раньше начала выделения —
+             * сбрасываем конец периода и начинаем выделение с этой даты
+             */
+            if (date < selectedFrom) {
+                setSelectedTo(undefined);
+                setSelectedFrom(date);
+            }
         },
         [resetPeriod, selectedFrom, selectedTo],
     );
