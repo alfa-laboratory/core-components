47756ca1d4eea89f78ed7234e95c02e51dd72e49
Dmitry Savkin <dmitrsavk@yandex.ru>
2022-02-16 16:56:22 +0300

    feat(date-input): add some improvements (#971)
    * feat(date-input): add some improvements
    
    * feat(date-input): some updates
    
    * feat(date-input): validation
    
    Co-authored-by: reme3d2y <AYatsenko@alfabank.ru>

--- packages/date-input/src/utils/format.ts
+++ packages/date-input/src/utils/format.ts
@@ -1,4 +1,4 @@
-import { parse, format } from 'date-fns';
+import { parse, format as dateFnsFormat, isValid as dateFnsIsValid } from 'date-fns';
 
 export const DATE_FORMAT = 'dd.MM.yyyy';
 export const NATIVE_DATE_FORMAT = 'yyyy-MM-dd';
@@ -7,7 +7,21 @@ export const DATE_MASK = [/\d/, /\d/, '.', /\d/, /\d/, '.', /\d/, /\d/, /\d/, /\
 export const isCompleteDateInput = (input: string) => input.length === DATE_MASK.length;
 
 export const formatDate = (date: Date | number, dateFormat = DATE_FORMAT) =>
-    format(date, dateFormat);
+    dateFnsFormat(date, dateFormat);
 
 export const parseDateString = (value: string, dateFormat = DATE_FORMAT) =>
     parse(value, dateFormat, new Date());
+
+export const isValid = (inputValue?: string) =>
+    !inputValue || (isCompleteDateInput(inputValue) && dateFnsIsValid(parseDateString(inputValue)));
+
+export const format = (value: string): string =>
+    value
+        .replace(/^(\d\d)(\d)$/, '$1.$2') // 121 => 12.1
+        .replace(/^(\d\d)\.(\d\d)(\d)$/, '$1.$2.$3') // 12.122 => 12.12.2
+        .replace(/^(\d\d)\d\.(.*)/, '$1.$2') // 123.12.2005 => 12.12.2005
+        .replace(/^(\d\d\.\d\d)\d\.(.*)/, '$1.$2') // 12.123.2005 => 12.12.2005
+        .replace(/^(\d\d\.\d\d\.\d\d\d\d).*/, '$1') // 12.12.20056 => 12.12.2005
+        .replace(/\.$/, '') // 12. => 12
+        .replace(/^(\d\d\.\d\d)(\d\d\d\d)/, '$1.$2') // 12.122005 => 12.12.2005
+        .replace(/^(\d\d)(\d\d\.\d\d\d\d)/, '$1.$2'); // 1212.2005 => 12.12.2005
