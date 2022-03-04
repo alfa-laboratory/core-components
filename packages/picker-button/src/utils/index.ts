0d23257b561329092be551d237389cc06c3d5607
Alexander Yatsenko <AYatsenko@alfabank.ru>
2022-03-04 18:53:29 +0300

    fix: move icon from select to picker-button (#1020)

--- packages/picker-button/src/utils/index.ts
+++ packages/picker-button/src/utils/index.ts
@@ -5,10 +5,13 @@ import { MoreMIcon } from '@alfalab/icons-glyph/MoreMIcon';
 import { MoreSIcon } from '@alfalab/icons-glyph/MoreSIcon';
 import { PickerButtonSize, PickerButtonVariant } from '..';
 
-export const getIcon = (variant: PickerButtonVariant, size: PickerButtonSize): FC<SVGProps<SVGSVGElement>> => {
-  if (variant === 'compact') {
-    return size === 'xxs' ? MoreSIcon : MoreMIcon;
-  }
+export const getIcon = (
+    variant: PickerButtonVariant,
+    size: PickerButtonSize,
+): FC<SVGProps<SVGSVGElement>> => {
+    if (variant === 'compact') {
+        return size === 'xxs' ? MoreSIcon : MoreMIcon;
+    }
 
-  return size === 'xxs' ? ChevronDownCompactSIcon : ChevronDownMIcon;
+    return size === 'xxs' ? ChevronDownCompactSIcon : ChevronDownMIcon;
 };
