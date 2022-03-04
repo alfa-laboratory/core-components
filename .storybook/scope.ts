e35163c7f1c38cc824a0d078bd0211bfdcc7b480
reme3d2y <AYatsenko@alfabank.ru>
2022-03-04 16:27:06 +0300

    fix(storybook): fix scope

--- .storybook/scope.ts
+++ .storybook/scope.ts
@@ -1,36 +1,31 @@
 import { ComponentType } from 'react';
 import * as dateUtils from 'date-fns';
-import * as glyph from '@alfalab/icons-glyph';
 import * as knobs from '@storybook/addon-knobs';
 import * as grid from './blocks/grid';
 
-const req = require.context(
+const componentsContext = require.context(
     '../packages',
     true,
     /^\.\/(.*)\/src\/(index|desktop|mobile|responsive|circle|super-ellipse).ts$/,
 );
 
-const isComponent = (component: any) =>
-    ['function', 'object'].some(t => typeof component === t) &&
-    ['displayName', '$$typeof'].some(p => p in component);
+const glyphContext = require.context('../node_modules/@alfalab/icons-glyph', true, /(.*).js$/);
 
-const isComponentsMap = (component: any) =>
-    component && Object.values(component).some(isComponent)
+const requireComponents = (context: __WebpackModuleApi.RequireContext) =>
+    context.keys().reduce((acc: Record<string, ComponentType<unknown>>, key) => {
+        Object.entries(context(key)).forEach(([componentName, component]: [string, any]) => {
+            if (['default', '__esModule'].includes(componentName)) return;
 
-const components = req.keys().reduce((acc: Record<string, ComponentType<unknown>>, key) => {
-    Object.entries(req(key)).forEach(([componentName, component]: [string, any]) => {
-        // if (isComponent(component) || isComponentsMap(component)) {
             acc[componentName] = component;
-        // }
-    });
+        });
 
-    return acc;
-}, {});
+        return acc;
+    }, {});
 
 export default {
-    ...components,
+    ...requireComponents(componentsContext),
+    ...requireComponents(glyphContext),
     ...grid,
-    ...glyph,
     ...dateUtils,
     ...knobs,
 };
