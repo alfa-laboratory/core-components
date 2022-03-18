391bd1421f96e50c692b1414fcd84a528e267e38
blackraydev <black2001ray@mail.ru>
2022-03-18 03:07:52 +0300

    chore(tooltip): added comment

--- packages/tooltip/src/component.mobile.tsx
+++ packages/tooltip/src/component.mobile.tsx
@@ -72,7 +72,7 @@ export const TooltipMobile: React.FC<TooltipMobileProps> = ({
             >
                 {content}
             </BottomSheet>
-            {/* jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
+            {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions */}
             <div onClick={handleOpen} className={styles.target}>
                 {children.props.disabled && <div className={styles.overlap} />}
                 {children}
