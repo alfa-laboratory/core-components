fdf2cdca9f785b27cd5d3998245a34d42e1240d1
Alexander Yatsenko <AYatsenko@alfabank.ru>
2022-03-24 12:05:40 +0300

    feat(modal): update styles (#1024)
    * feat(modal): update styles
    
    * fix(modal): pr fixes
    
    * fix(modal): pr fixes
    
    * fix(modal): pr fixes
    
    * test(modal): update screenshots
    
    * fix(modal): pr fixes
    
    * fix(modal): pr fixes
    
    * fix(modal): pr fixes
    
    Breaking change: Большое обновление стилей и темизации
    
    * fix(modal): remove unused align

--- packages/modal/src/components/content/Component.mobile.tsx
+++ packages/modal/src/components/content/Component.mobile.tsx
@@ -1,10 +1,11 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 import { Content, ContentProps } from './Component';
 
 import styles from './mobile.module.css';
 
 export type ContentMobileProps = ContentProps;
 
-export const ContentMobile: FC<ContentMobileProps> = props => (
-    <Content styles={styles} {...props} />
+export const ContentMobile: FC<ContentMobileProps> = ({ className, ...restProps }) => (
+    <Content className={cn(className, styles.content)} {...restProps} />
 );
